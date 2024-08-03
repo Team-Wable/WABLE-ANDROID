9981dd346a8ec265953954ea31b97008310883ee
Sohyun <so405@naver.com>
2024-08-04 06:52:02 +0900

    #1 [SETTING] : init feature:notification module

---
+++ feature/notification/src/main/java/com/teamwable/notification/NotificationFragment.kt
@@ -0,0 +1,11 @@
+package com.teamwable.notification
+
+import com.teamwable.notification.databinding.FragmentNotificationBinding
+import com.teamwable.ui.base.BindingFragment
+
+class NotificationFragment :
+    BindingFragment<FragmentNotificationBinding>(FragmentNotificationBinding::inflate) {
+    override fun initView() {
+
+    }
+}
