2a81ce0b3c8b4a12ddcf0fdf108e3278516f48c0
Sohyun <so405@naver.com>
2024-08-04 06:55:39 +0900

    #1 [SETTING] : init feature:profile module

---
+++ feature/profile/src/main/java/com/teamwable/profile/ProfileFragment.kt
@@ -0,0 +1,10 @@
+package com.teamwable.profile
+
+import com.teamwable.profile.databinding.FragmentProfileBinding
+import com.teamwable.ui.base.BindingFragment
+
+class ProfileFragment : BindingFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
+    override fun initView() {
+
+    }
+}
