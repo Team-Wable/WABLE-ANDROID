4c03bc89a509ecaa98d55c00a80afcd1c6261e73
Sohyun <so405@naver.com>
2024-08-04 06:44:46 +0900

    #1 [SETTING] : init feature:posting module

---
+++ feature/posting/build.gradle.kts
@@ -0,0 +1,26 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    id("com.teamwable.wable.feature")
+    id("com.teamwable.wable.test")
+}
+android {
+    namespace = "com.teamwable.posting"
+}
+
+dependencies {
+    implementation(project(":core:ui"))
+    implementation(project(":core:model"))
+    implementation(project(":core:data"))
+
+    // AndroidX
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.fragment.ktx)
+    implementation(libs.splash.screen)
+
+    // Third Party
+    implementation(libs.coil.core)
+    implementation(libs.material)
+    implementation(libs.androidx.appcompat)
+}
---
+++ feature/posting/src/main/java/com/teamwable/posting/PostingFragment.kt
@@ -0,0 +1,10 @@
+package com.teamwable.posting
+
+import com.teamwable.posting.databinding.FragmentPostingBinding
+import com.teamwable.ui.base.BindingFragment
+
+class PostingFragment : BindingFragment<FragmentPostingBinding>(FragmentPostingBinding::inflate) {
+    override fun initView() {
+
+    }
+}
