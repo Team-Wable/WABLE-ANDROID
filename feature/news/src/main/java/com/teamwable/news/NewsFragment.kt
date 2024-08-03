ecddf8821a1661d9823e1326b29a03e7fc414425
Sohyun <so405@naver.com>
2024-08-04 06:48:57 +0900

    #1 [SETTING] : init feature:news module

---
+++ feature/news/src/main/java/com/teamwable/news/NewsFragment.kt
@@ -0,0 +1,10 @@
+package com.teamwable.news
+
+import com.teamwable.news.databinding.FragmentNewsBinding
+import com.teamwable.ui.base.BindingFragment
+
+class NewsFragment : BindingFragment<FragmentNewsBinding>(FragmentNewsBinding::inflate) {
+    override fun initView() {
+
+    }
+}
