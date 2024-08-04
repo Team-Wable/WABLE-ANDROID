a8951eeb6c667f30ee18cf0890da84347cf1a227
Sohyun <so405@naver.com>
2024-08-04 23:23:13 +0900

    #1 [SETTING] : fix default dummy repo

--- core/data/src/main/java/com/teamwable/data/repositoryimpl/DefaultDummyRepository.kt
+++ core/data/src/main/java/com/teamwable/data/repositoryimpl/DefaultDummyRepository.kt
@@ -3,24 +3,17 @@ package com.teamwable.data.repositoryimpl
 import com.teamwable.data.mapper.toModel.toDummy
 import com.teamwable.data.remote.datasource.DummyService
 import com.teamwable.data.repository.DummyRepository
-import com.teamwable.data.util.EndPoints.ERROR
 import com.teamwable.model.Dummy
-import java.net.HttpURLConnection
 import javax.inject.Inject
 
-internal class DefaultDummyRepository
+class DefaultDummyRepository
 @Inject
 constructor(
     private val dummyService: DummyService,
 ) : DummyRepository {
     override suspend fun getDummy(): Result<List<Dummy>> {
         return runCatching {
-            val response = dummyService.getDummy()
-            if (response.status == HttpURLConnection.HTTP_OK) {
-                response.data?.map { it.toDummy() } ?: emptyList()
-            } else {
-                throw Exception("${ERROR}${response.status}")
-            }
+            dummyService.getDummy().data?.map { it.toDummy() } ?: emptyList()
         }
     }
 }
