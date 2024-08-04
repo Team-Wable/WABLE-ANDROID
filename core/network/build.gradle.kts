1276d323a5115949b8a73ae78a898f2bacaca350
Sohyun <so405@naver.com>
2024-08-04 23:44:58 +0900

    #1 [SETTING] : move firebase dependency network -> app

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,7 +1,6 @@
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     id("com.teamwable.wable.feature")
-    alias(libs.plugins.google.services)
 }
 android {
     namespace = "com.teamwable.network"
@@ -9,8 +8,4 @@ android {
 
 dependencies {
     implementation(project(":core:model"))
-
-    //firebase
-    implementation(platform(libs.firebase.bom))
-    implementation(libs.firebase.analytics)
 }
