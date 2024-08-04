8550a258e5bbbf517a89b426dd49757bd653573d
Sohyun <so405@naver.com>
2024-08-04 14:52:24 +0900

    #1 [SETTING] : set firebase dependency

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,6 +1,7 @@
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     id("com.teamwable.wable.feature")
+    alias(libs.plugins.google.services)
 }
 android {
     namespace = "com.teamwable.network"
@@ -8,4 +9,8 @@ android {
 
 dependencies {
     implementation(project(":core:model"))
+
+    //firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
 }
