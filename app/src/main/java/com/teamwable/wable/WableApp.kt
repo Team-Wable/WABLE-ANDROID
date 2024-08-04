d71d3d1027ef10c77d8ce47a31498f48766a0bf7
Sohyun <so405@naver.com>
2024-08-05 08:37:17 +0900

    #1 [SETTING] : fix hilt deprecation warning
    https://github.com/google/dagger/issues/3601

--- app/src/main/java/com/teamwable/wable/WableApp.kt
+++ app/src/main/java/com/teamwable/wable/WableApp.kt
@@ -1,11 +1,18 @@
 package com.teamwable.wable
 
 import android.app.Application
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.qualifiers.ApplicationContext
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
 class WableApp : Application() {
+    @Inject
+    @ApplicationContext
+    lateinit var context: Context
+
     override fun onCreate() {
         super.onCreate()
         setTimber()
