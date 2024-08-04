9b2eea79a53ba9d415be9a432a3761b029ac9049
Sohyun <so405@naver.com>
2024-08-04 10:59:21 +0900

    #1 [SETTING] : replace kapt to ksp at hilt plugin

--- build-logic/src/main/java/com/teamwable/wable/plugin/AndroidHiltPlugin.kt
+++ build-logic/src/main/java/com/teamwable/wable/plugin/AndroidHiltPlugin.kt
@@ -9,16 +9,16 @@ import org.gradle.kotlin.dsl.getByType
 class AndroidHiltPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
         with(plugins) {
-            apply("kotlin-kapt")
+            apply("com.google.devtools.ksp")
             apply("com.google.dagger.hilt.android")
         }
 
         val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
         dependencies {
             "implementation"(libs.findLibrary("hilt").get())
-            "kapt"(libs.findLibrary("hilt.compiler").get())
+            "ksp"(libs.findLibrary("hilt.compiler").get())
             "testImplementation"(libs.findLibrary("hilt.testing").get())
-            "kaptTest"(libs.findLibrary("hilt.testing.compiler").get())
+            "kspTest"(libs.findLibrary("hilt.testing.compiler").get())
         }
     }
 }
