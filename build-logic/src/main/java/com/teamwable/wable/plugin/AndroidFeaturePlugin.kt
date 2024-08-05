af8ae309e3b86d8e0936ee6f0b64401267428a49
Sohyun <so405@naver.com>
2024-08-05 09:10:27 +0900

    #1 [SETTING] : fix feature dependency

--- build-logic/src/main/java/com/teamwable/wable/plugin/AndroidFeaturePlugin.kt
+++ build-logic/src/main/java/com/teamwable/wable/plugin/AndroidFeaturePlugin.kt
@@ -2,10 +2,18 @@ package com.teamwable.wable.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
 
 class AndroidFeaturePlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
         plugins.apply("com.android.library")
         configureAndroidCommonPlugin()
+
+        val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+        dependencies {
+            "implementation"(libs.findLibrary("androidx.constraintlayout").get())
+        }
     }
 }
