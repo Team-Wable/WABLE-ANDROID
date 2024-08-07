fd5528255462452f46b09052cf8d3166dad2e129
chanu <cw8881@naver.com>
2024-08-07 18:32:40 +0900

    #3 [setting] : auth 멀티모듈 생성

---
+++ feature/auth/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("com.teamwable.wable.compose")
+    alias(libs.plugins.ktlint)
+}
+
+android {
+    namespace = "com.teamwable.auth"
+}
+
+dependencies {
+}
+
+ktlint {
+    version = "0.49.1"
+    android.set(true)
+    debug.set(true)
+    coloredOutput.set(true)
+    verbose.set(true)
+    outputToConsole.set(true)
+}
