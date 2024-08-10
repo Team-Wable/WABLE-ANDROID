7b87e57cc1bdb6331ff12fe20e9f8cbf5ba26bed
Sohyun <so405@naver.com>
2024-08-10 17:40:52 +0900

    #10 [DEL] : remove test token

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -16,10 +16,6 @@ android {
         getByName("debug") {
             val wableDevBaseUrl = properties["wable.dev.base.url"] as? String ?: ""
             buildConfigField("String", "WABLE_BASE_URL", "\"${wableDevBaseUrl}\"")
-
-            //TODO:테스트용 나중에 지우기
-            val testToken = properties["test.token"] as? String ?: ""
-            buildConfigField("String", "TEST_TOKEN", "\"${testToken}\"")
         }
 
         getByName("release") {
--- core/network/src/main/java/com/teamwable/network/datasource/DummyService.kt
+++ core/network/src/main/java/com/teamwable/network/datasource/DummyService.kt
@@ -1,6 +1,5 @@
 package com.teamwable.network.datasource
 
-import com.teamwable.network.BuildConfig.TEST_TOKEN
 import com.teamwable.network.dto.response.ResponseDummyDto
 import com.teamwable.network.util.BaseResponse
 import retrofit2.http.GET
@@ -8,7 +7,7 @@ import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface DummyService {
-    @Headers("Authorization: Bearer $TEST_TOKEN")
+    @Headers("Authorization: Bearer ")
     @GET("api/v2/contents")
     suspend fun getDummy(
         @Query(value = "cursor") contentId: Long = -1,
