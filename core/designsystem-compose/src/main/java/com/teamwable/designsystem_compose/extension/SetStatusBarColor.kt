0a5a671de720df3c057507d4e864e64fda6982bc
chanu <cw8881@naver.com>
2024-08-11 05:49:26 +0900

    #3 [feat] : SetStatusBarColor util 함수 추가

---
+++ core/designsystem-compose/src/main/java/com/teamwable/designsystem_compose/extension/SetStatusBarColor.kt
@@ -0,0 +1,21 @@
+package com.teamwable.designsystem_compose.extension
+
+import android.app.Activity
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.view.WindowCompat
+
+@Composable
+fun SetStatusBarColor(color: Int) {
+    val activity = LocalContext.current as? Activity
+    SideEffect {
+        activity?.window?.let { window ->
+            WindowCompat.getInsetsController(
+                window,
+                window.decorView,
+            ).isAppearanceLightStatusBars = false
+            window.statusBarColor = color
+        }
+    }
+}
