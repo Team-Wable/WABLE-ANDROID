a8cb766f53ae5599a1e1f9120c359e8fbf9361fe
chanu <cw8881@naver.com>
2024-08-11 05:48:45 +0900

    #3 [feat] : splash graph intent provider 주입

--- feature/main-compose/src/main/java/com/teamwable/main_compose/splash/navigation/SplashNavigation.kt
+++ feature/main-compose/src/main/java/com/teamwable/main_compose/splash/navigation/SplashNavigation.kt
@@ -2,16 +2,19 @@ package com.teamwable.main_compose.splash.navigation
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.teamwable.common.intentprovider.IntentProvider
 import com.teamwable.navigation.Route
 
 fun NavGraphBuilder.splashNavGraph(
     navigateToLogIn: () -> Unit,
     navigateToHome: () -> Unit,
+    intentProvider: IntentProvider,
 ) {
     composable<Route.Splash> {
         /*  SplashScreen(
               navigateToHome = navigateToHome,
               navigateToLogIn = navigateToLogIn
+              intentProvider: IntentProvider,
           )*/
     }
 }
