de13ab12d8ad94bccc5a8c64cbbdc81b2acce5e6
chanu <cw8881@naver.com>
2024-08-10 20:25:57 +0900

    #3 [setting] : compose main activity setting

---
+++ feature/main-compose/src/main/java/com/teamwable/main_compose/ComposeMainActivity.kt
@@ -0,0 +1,20 @@
+package com.teamwable.main_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.teamwable.designsystem_compose.theme.WableTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ComposeMainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WableTheme {
+                val navigator: MainNavigator = rememberMainNavigator()
+                MainScreen(navigator = navigator)
+            }
+        }
+    }
+}
