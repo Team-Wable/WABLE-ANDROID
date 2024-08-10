efb55d9af497c0f9c84dbdcc0db56c4d03dd7603
chanu <cw8881@naver.com>
2024-08-11 06:07:17 +0900

    #3 [rename] : 기본 theme status bar 색상 설정

--- core/designsystem/src/main/java/com/teamwable/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/teamwable/designsystem/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -55,8 +56,8 @@ fun WableTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = Color.Transparent.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
         }
     }
 
