f4077f568b7400152ef4b34fd9afce99be1ead07
chanu <cw8881@naver.com>
2024-08-11 05:13:21 +0900

    #3 [feat] : intent provider 의존성 필드 주입

--- feature/main-compose/src/main/java/com/teamwable/main_compose/MainComposeActivity.kt
+++ feature/main-compose/src/main/java/com/teamwable/main_compose/MainComposeActivity.kt
@@ -3,17 +3,27 @@ package com.teamwable.main_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.teamwable.common.di.MAIN
+import com.teamwable.common.intentprovider.IntentProvider
 import com.teamwable.designsystem_compose.theme.WableTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainComposeActivity : ComponentActivity() {
+    @MAIN
+    @Inject
+    lateinit var intentProvider: IntentProvider
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             WableTheme {
                 val navigator: MainNavigator = rememberMainNavigator()
-                MainScreen(navigator = navigator)
+                MainScreen(
+                    navigator = navigator,
+                    intentProvider = intentProvider,
+                )
             }
         }
     }
