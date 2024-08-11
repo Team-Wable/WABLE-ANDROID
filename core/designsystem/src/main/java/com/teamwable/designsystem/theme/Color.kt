ba7b3e969f79da4f4831e6d7b4236c2be3b3e80f
chanu <cw8881@naver.com>
2024-08-11 17:57:13 +0900

    #15 [setting] : compose color setting

--- core/designsystem/src/main/java/com/teamwable/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/teamwable/designsystem/theme/Color.kt
@@ -2,10 +2,52 @@ package com.teamwable.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Primary
+val Purple50 = Color(0xFF8F4BFF)
+val Purple100 = Color(0xFF3D059A)
+val Purple10 = Color(0xFFF4EDFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Secondary
+val Sky50 = Color(0xFF05E8D1)
+val Sky10 = Color(0xFFE4FAF8)
+
+// System
+val Success = Color(0xFF0DBE61)
+val Info = Color(0xFF127CF4)
+val Warning = Color(0xFFEE9209)
+val Error = Color(0xFFF01F1F)
+
+// Gray Scale
+val Black = Color(0xFF0E0E0E)
+val Gray900 = Color(0xFF2D2D2D)
+val Gray800 = Color(0xFF4A4A4A)
+val Gray700 = Color(0xFF6B6B6B)
+val Gray600 = Color(0xFF8D8D8D)
+val Gray500 = Color(0xFFAEAEAE)
+val Gray400 = Color(0xFFCCCCCC)
+val Gray300 = Color(0xFFDEDEDE)
+val Gray200 = Color(0xFFEDEDED)
+val Gray100 = Color(0xFFF7F7F7)
+val White = Color(0xFFFDFDFD)
+
+// Team Colors
+val T50 = Color(0xFFE2012D)
+val T10 = Color(0xFFF9E3E7)
+val Gen50 = Color(0xFFAA8B30)
+val Gen10 = Color(0xFFF4F1E8)
+val Bro50 = Color(0xFF01492B)
+val Bro10 = Color(0xFFFAEBEB)
+val Drx50 = Color(0xFF1004A4)
+val Drx10 = Color(0xFFE4E3F3)
+val Dk50 = Color(0xFF4A4A4A)
+val Dk10 = Color(0xFFEDEDED)
+val Kt50 = Color(0xFFFF0A09)
+val Kt10 = Color(0xFFFCE4E4)
+val Fox50 = Color(0xFFD31F28)
+val Fox10 = Color(0xFFFBF8DB)
+val Ns50 = Color(0xFFEC1C24)
+val Ns10 = Color(0xFFE3E3E3)
+val Kdf50 = Color(0xFFE63312)
+val Kdf10 = Color(0xFFFAE8E5)
+val Hle50 = Color(0xFFF47320)
+val Hle10 = Color(0xFFFBEEE6)
