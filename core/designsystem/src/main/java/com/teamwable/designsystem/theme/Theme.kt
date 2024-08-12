77c2c04aecb8576efd0a300e027ad33ef3b0a14b
chanu <cw8881@naver.com>
2024-08-12 12:59:11 +0900

    #15 [setting] : wable custom theme composition local setting

--- core/designsystem/src/main/java/com/teamwable/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/teamwable/designsystem/theme/Theme.kt
@@ -1,69 +1,65 @@
 package com.teamwable.designsystem.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
+import com.teamwable.designsystem.extension.system.SetLightNavigationBar
+import com.teamwable.designsystem.extension.system.SetStatusBarColor
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
-)
+private val LocalWableColors = staticCompositionLocalOf<WableColors> {
+    error("No WableColors provided")
+}
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-     */
-)
+private val LocalWableTypography = staticCompositionLocalOf<WableTypography> {
+    error("No WableTypography provided")
+}
+
+/*
+* SoptTheme
+*
+* Color에 접근하고 싶을때 WableTheme.colors.primary 이런식으로 접근하면 됩니다.
+* Typo를 변경하고 싶다면 WableTheme.typography.h1 이런식으로 접근하면 됩니다.
+* */
+
+object WableTheme {
+    val colors: WableColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalWableColors.current
+
+    val typography: WableTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalWableTypography.current
+}
+
+@Composable
+fun ProvideSoptColorsAndTypography(colors: WableColors, typography: WableTypography, content: @Composable () -> Unit) {
+    val provideColors = remember { colors.copy() }
+    provideColors.update(colors)
+
+    val provideTypography = remember { typography.copy() }
+    provideTypography.update(typography)
+
+    CompositionLocalProvider(
+        LocalWableColors provides provideColors,
+        LocalWableTypography provides provideTypography,
+        content = content,
+    )
+}
 
 @Composable
 fun WableTheme(
-    darkTheme: Boolean = false,
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,
     content: @Composable () -> Unit,
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colors = wableLightColors()
+    val typography = wableTypography()
+    SetStatusBarColor(color = colors.white)
+    SetLightNavigationBar()
+    ProvideSoptColorsAndTypography(colors, typography) {
+        MaterialTheme(content = content)
     }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = Color.Transparent.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
-        }
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content,
-    )
 }
