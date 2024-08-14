33a36756605de6133c9dc1dcf72d232352ea748f
Eonji-sw <ejkim0625@gmail.com>
2024-08-15 03:26:14 +0900

    #18 [FEAT] : profile fragment UI 구현

--- feature/profile/src/main/java/com/teamwable/profile/ProfileFragment.kt
+++ feature/profile/src/main/java/com/teamwable/profile/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.teamwable.profile
 
+import android.view.View
 import com.teamwable.profile.databinding.FragmentProfileBinding
 import com.teamwable.ui.base.BindingFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -7,5 +8,29 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class ProfileFragment : BindingFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
     override fun initView() {
+        initAppbarBtnVisibility()
+        setAppbarText()
+        initAppbarHamburgerClickListener()
+    }
+
+    private fun setAppbarText() {
+        binding.viewProfileAppbar.tvProfileAppbarTitle.text = "배 차은우"
+    }
+
+    private fun initAppbarBtnVisibility() {
+        with(binding.viewProfileAppbar) {
+            btnProfileAppbarBack.visibility = View.GONE
+            btnProfileAppbarHamburger.visibility = View.VISIBLE
+        }
+    }
+
+    private fun initAppbarHamburgerClickListener() {
+        binding.viewProfileAppbar.btnProfileAppbarHamburger.setOnClickListener {
+            ProfileHamburgerBottomSheet().show(childFragmentManager, PROFILE_HAMBURGER_BOTTOM_SHEET)
+        }
+    }
+
+    companion object {
+        const val PROFILE_HAMBURGER_BOTTOM_SHEET = "ProfileHamburgerBottomSheet"
     }
 }
