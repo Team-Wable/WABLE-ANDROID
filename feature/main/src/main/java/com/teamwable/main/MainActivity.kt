793ce3641bf592ab5098a20ba63ea829d8568cf3
Sohyun <so405@naver.com>
2024-08-14 16:19:03 +0900

    #16 [FEAT] : set up main bnv

--- feature/main/src/main/java/com/teamwable/main/MainActivity.kt
+++ feature/main/src/main/java/com/teamwable/main/MainActivity.kt
@@ -5,18 +5,19 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.teamwable.main.databinding.ActivityMainBinding
+import com.teamwable.ui.extensions.colorOf
 import com.teamwable.ui.extensions.visible
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: DummyViewModel by viewModels()
 
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,17 +36,41 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.container_main) as NavHostFragment
         val navController = navHostFragment.navController
-        binding.bnvMain.setupWithNavController(navController)
-        navController.addOnDestinationChangedListener { controller, destination, arguments ->
-            when (destination.id) {
-                R.id.navigation_posting -> {
-                    binding.groupMainBnv.visible(false)
-                }
 
-                else -> {
-                    binding.groupMainBnv.visible(true)
-                }
+        //TODO : 나중에 BADGE 보이게 하는 로직으로 이동
+        setBadgeOnNotification(navController, true)
+
+        with(binding) {
+            bnvMain.itemIconTintList = null
+            bnvMain.setupWithNavController(navController)
+        }
+        initBottomNavigationChangedListener(navController)
+    }
+
+    private fun initBottomNavigationChangedListener(navController: NavController) {
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            handleBottomNavigationVisibility(navController, destination)
+            if (destination.id == R.id.navigation_notification) setBadgeOnNotification(navController, false)
+        }
+    }
+
+    private fun handleBottomNavigationVisibility(navController: NavController, destination: NavDestination) {
+        when (destination.id) {
+            R.id.navigation_posting -> {
+                binding.groupMainBnv.visible(false)
             }
+
+            else -> {
+                binding.groupMainBnv.visible(true)
+            }
+        }
+    }
+
+    private fun setBadgeOnNotification(navController: NavController, isVisible: Boolean) {
+        binding.bnvMain.getOrCreateBadge(R.id.navigation_notification).apply {
+            this.isVisible = isVisible
+            horizontalOffset = 1
+            if (isVisible) backgroundColor = colorOf(com.teamwable.ui.R.color.error) else clearNumber()
         }
     }
 
