7e6952868cb5a10420b827699dbc2700c8d85821
chanu <cw8881@naver.com>
2024-08-17 00:17:59 +0900

    #22 [setting] : datastore 모듈 생성

---
+++ core/datastore/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.android.library)
+    id("com.teamwable.wable.kotlin")
+    id("com.teamwable.wable.hilt")
+}
+
+android {
+    namespace = "com.teamwable.datastore"
+}
+
+dependencies {
+}
