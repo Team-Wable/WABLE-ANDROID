066cefa353b5542e6ab57bec225650b4c9f5986e
Eonji-sw <ejkim0625@gmail.com>
2024-08-17 20:19:43 +0900

    #20 [MOD] : viewModel mockData 수정

--- feature/news/src/main/java/com/teamwable/news/NewsViewModel.kt
+++ feature/news/src/main/java/com/teamwable/news/NewsViewModel.kt
@@ -2,6 +2,7 @@ package com.teamwable.news
 
 import androidx.lifecycle.ViewModel
 import com.teamwable.model.NewsMatchModel
+import com.teamwable.model.NewsMatchScoreModel
 import com.teamwable.model.NewsRankModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -10,10 +11,23 @@ import javax.inject.Inject
 class NewsViewModel
 @Inject constructor() : ViewModel() {
     val mockNewsMatchList = listOf(
-        NewsMatchModel("08. 17 (토)", "T1", 2, "GEN", 3, "종료"),
-        NewsMatchModel("08. 17 (토)", "FOX", 1, "BRO", 0, "진행중"),
-        NewsMatchModel("08. 19 (월)", "T1", 2, "GEN", 3, "예정"),
+        NewsMatchModel(
+            "2024-08-17",
+            listOf(
+                NewsMatchScoreModel("2024-08-17 16:07", "T1", 2, "GEN", 3, "TERMINATION"),
+                NewsMatchScoreModel("2024-08-17 20:00", "FOX", 1, "BRO", 0, "PROGRESS"),
+            )
+        ),
+        NewsMatchModel(
+            "2024-08-20",
+            listOf(NewsMatchScoreModel("2024-08-20 09:50", "T1", 2, "GEN", 3, "SCHEDULED"))
+        ),
+        NewsMatchModel(
+            "2024-08-21",
+            listOf(NewsMatchScoreModel("2024-08-21 12:25", "T1", 2, "GEN", 3, "SCHEDULED"))
+        ),
     )
+    val mockNewsMatchEmptyList = emptyList<NewsMatchModel>()
 
     val mockNewsRankList = listOf(
         NewsRankModel(1, "T1", 8, 7, 8, 0),
