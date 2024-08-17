49f1da6d7cf2de52cf8a6c37b3fffb5a384f35cd
Eonji-sw <ejkim0625@gmail.com>
2024-08-17 20:18:59 +0900

    #20 [MOD] : newsMatchModel 수정

--- core/model/src/main/java/com/teamwable/model/NewsMatchModel.kt
+++ core/model/src/main/java/com/teamwable/model/NewsMatchModel.kt
@@ -1,10 +1,6 @@
 package com.teamwable.model
 
 data class NewsMatchModel(
-    val gameDate: String,
-    val aTeamName: String,
-    val aTeamScore: Int,
-    val bTeamName: String,
-    val bTeamScore: Int,
-    val gameStatus: String,
+    val date: String,
+    val games: List<NewsMatchScoreModel>,
 )
