561553504494758c1464628cab2d93b2a28e141f
Eonji-sw <ejkim0625@gmail.com>
2024-08-18 02:19:27 +0900

    #28 [FEAT] : notification information fragment 구현

---
+++ feature/notification/src/main/java/com/teamwable/notification/information/NotificationInformationFragment.kt
@@ -0,0 +1,39 @@
+package com.teamwable.notification.information
+
+import androidx.fragment.app.viewModels
+import com.teamwable.notification.NotificationItemDecorator
+import com.teamwable.notification.NotificationViewModel
+import com.teamwable.notification.databinding.FragmentNotificationVpBinding
+import com.teamwable.ui.base.BindingFragment
+import com.teamwable.ui.extensions.toast
+import com.teamwable.ui.extensions.visible
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NotificationInformationFragment : BindingFragment<FragmentNotificationVpBinding>(FragmentNotificationVpBinding::inflate) {
+    private val viewModel: NotificationViewModel by viewModels()
+
+    override fun initView() {
+        initNotificationInformationAdapter()
+    }
+
+    private fun initNotificationInformationAdapter() {
+        if (viewModel.mockNotificationInformationList.isEmpty()) {
+            binding.llNotificationVpEmpty.visible(true)
+        } else {
+            binding.llNotificationVpEmpty.visible(false)
+
+            binding.rvNotificationContent.adapter =
+                NotificationInformationAdapter(click = { notificationInformationData, position ->
+                    when (notificationInformationData.infoNotificationType) {
+                        "GAMEDONE" -> toast("GAMEDONE")
+                        "GAMESTART" -> toast("GAMESTART")
+                        "WEEKDONE" -> toast("WEEKDONE")
+                    }
+                }).apply {
+                    submitList(viewModel.mockNotificationInformationList)
+                }
+            binding.rvNotificationContent.addItemDecoration(NotificationItemDecorator(requireContext()))
+        }
+    }
+}
