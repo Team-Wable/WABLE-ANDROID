f2fc7df20e3d1db55b2995912f61af5291de7972
chanu <cw8881@naver.com>
2024-08-17 19:09:05 +0900

    #24 [setting] : network error handling function setting

---
+++ core/network/src/main/java/com/teamwable/network/util/getErrorMessage.kt
@@ -0,0 +1,33 @@
+package com.teamwable.network.util
+
+import com.teamwable.model.network.ApiError
+import com.teamwable.model.network.NetWorkConnectError
+import kotlinx.serialization.json.Json
+import retrofit2.HttpException
+import java.net.UnknownHostException
+
+private const val UNKNOWN_ERROR_MESSAGE = "Unknown error"
+
+private fun HttpException.getErrorMessage(): String {
+    val errorBody = response()?.errorBody()?.string() ?: return UNKNOWN_ERROR_MESSAGE
+    return parseErrorMessage(errorBody)
+}
+
+private fun parseErrorMessage(errorBody: String): String {
+    return try {
+        val errorResponse = Json.decodeFromString<BaseResponse<Unit>>(errorBody)
+        errorResponse.message
+    } catch (e: Exception) {
+        UNKNOWN_ERROR_MESSAGE
+    }
+}
+
+fun <T> Throwable.handleThrowable(): Result<T> {
+    return Result.failure(
+        when (this) {
+            is HttpException -> ApiError(this.getErrorMessage())
+            is UnknownHostException -> NetWorkConnectError("인터넷에 연결해 주세요")
+            else -> this
+        },
+    )
+}
