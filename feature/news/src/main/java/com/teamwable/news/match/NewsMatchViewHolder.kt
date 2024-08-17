8665eb08bd9b124e350723cb5bb9fb9155fc1aa6
Eonji-sw <ejkim0625@gmail.com>
2024-08-17 17:28:10 +0900

    #20 [FEAT] : news match viewHolder 구현

---
+++ feature/news/src/main/java/com/teamwable/news/match/NewsMatchViewHolder.kt
@@ -0,0 +1,14 @@
+package com.teamwable.news.match
+
+import androidx.recyclerview.widget.RecyclerView
+import com.teamwable.model.NewsMatchModel
+import com.teamwable.news.databinding.ItemNewsMatchBinding
+
+class NewsMatchViewHolder(
+    private val binding: ItemNewsMatchBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(data: NewsMatchModel) {
+        with(binding) {
+        }
+    }
+}
