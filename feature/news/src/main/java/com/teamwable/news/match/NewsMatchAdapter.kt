71f9873239d133556ec0c40a40dd966c6af5c453
Eonji-sw <ejkim0625@gmail.com>
2024-08-17 20:17:26 +0900

    #20 [MOD] : DiffCallback 수정

--- feature/news/src/main/java/com/teamwable/news/match/NewsMatchAdapter.kt
+++ feature/news/src/main/java/com/teamwable/news/match/NewsMatchAdapter.kt
@@ -1,13 +1,15 @@
 package com.teamwable.news.match
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.ListAdapter
 import com.teamwable.model.NewsMatchModel
 import com.teamwable.news.databinding.ItemNewsMatchBinding
 import com.teamwable.ui.extensions.ItemDiffCallback
 
-class NewsMatchAdapter() : ListAdapter<NewsMatchModel, NewsMatchViewHolder>(
+class NewsMatchAdapter : ListAdapter<NewsMatchModel, NewsMatchViewHolder>(
     NewsMatchAdapterDiffCallback,
 ) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsMatchViewHolder {
@@ -15,6 +17,7 @@ class NewsMatchAdapter() : ListAdapter<NewsMatchModel, NewsMatchViewHolder>(
         return NewsMatchViewHolder(binding)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: NewsMatchViewHolder, position: Int) {
         holder.bind(currentList[position])
     }
@@ -22,7 +25,7 @@ class NewsMatchAdapter() : ListAdapter<NewsMatchModel, NewsMatchViewHolder>(
     companion object {
         private val NewsMatchAdapterDiffCallback =
             ItemDiffCallback<NewsMatchModel>(
-                onItemsTheSame = { old, new -> old.aTeamName == new.aTeamName },
+                onItemsTheSame = { old, new -> old.date == new.date },
                 onContentsTheSame = { old, new -> old == new },
             )
     }
