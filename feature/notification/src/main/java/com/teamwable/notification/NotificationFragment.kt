3309c931ea0ab4cb8aa7fc34b4f7f4ab956e0c11
Eonji-sw <ejkim0625@gmail.com>
2024-08-18 02:18:34 +0900

    #28 [FEAT] : notification tabLayout, viewPager 연결

--- feature/notification/src/main/java/com/teamwable/notification/NotificationFragment.kt
+++ feature/notification/src/main/java/com/teamwable/notification/NotificationFragment.kt
@@ -1,12 +1,27 @@
 package com.teamwable.notification
 
+import com.google.android.material.tabs.TabLayoutMediator
 import com.teamwable.notification.databinding.FragmentNotificationBinding
 import com.teamwable.ui.base.BindingFragment
+import com.teamwable.ui.extensions.stringOf
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class NotificationFragment :
     BindingFragment<FragmentNotificationBinding>(FragmentNotificationBinding::inflate) {
     override fun initView() {
+        initNotificationViewPagerAdapter()
+    }
+
+    private fun initNotificationViewPagerAdapter() {
+        with(binding) {
+            vpNotification.adapter = NotificationViewPagerAdapter(this@NotificationFragment)
+            TabLayoutMediator(tlNotification, vpNotification) { tab, position ->
+                when (position) {
+                    0 -> tab.text = stringOf(R.string.tv_notification_tab_action)
+                    1 -> tab.text = stringOf(R.string.tv_notification_tab_information)
+                }
+            }.attach()
+        }
     }
 }
