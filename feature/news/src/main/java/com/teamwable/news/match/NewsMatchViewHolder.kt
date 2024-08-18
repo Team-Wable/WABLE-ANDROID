ea9b5856a2af407b2f4a8dd365ff77880a742b4d
Eonji-sw <ejkim0625@gmail.com>
2024-08-18 16:25:42 +0900

    #20 [MOD] : calculateTime 적용

--- feature/news/src/main/java/com/teamwable/news/match/NewsMatchViewHolder.kt
+++ feature/news/src/main/java/com/teamwable/news/match/NewsMatchViewHolder.kt
@@ -6,10 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.teamwable.model.NewsMatchModel
 import com.teamwable.news.databinding.ItemNewsMatchBinding
 import com.teamwable.ui.extensions.visible
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import java.time.format.TextStyle
-import java.util.Locale
+import com.teamwable.ui.util.CalculateTime
 
 class NewsMatchViewHolder(
     private val binding: ItemNewsMatchBinding
@@ -17,28 +14,11 @@ class NewsMatchViewHolder(
     @RequiresApi(Build.VERSION_CODES.O)
     fun bind(data: NewsMatchModel) {
         with(binding) {
-            tvNewsMatchToday.visible(isToday(data.date))
-            tvNewsMatchDate.text = formatDate(data.date)
+            tvNewsMatchToday.visible(CalculateTime().isToday(data.date))
+            tvNewsMatchDate.text = CalculateTime().formatDate(data.date)
 
-            rvNewsMatchScore.adapter = NewsMatchScoreAdapter().apply { submitList(data.games) }
+            rvNewsMatchScore.adapter = NewsMatchScoreAdapter(root.context).apply { submitList(data.games) }
             rvNewsMatchScore.addItemDecoration(NewsMatchScoreItemDecorator(binding.root.context))
         }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun isToday(inputDate: String): Boolean {
-        val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-        return LocalDate.parse(inputDate, dateFormatter).isEqual(LocalDate.now())
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun formatDate(inputDate: String): String {
-        val inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-        val outputFormatter = DateTimeFormatter.ofPattern("MM. dd (E)")
-
-        val date = LocalDate.parse(inputDate, inputFormatter)
-        val koreanDayOfWeek = date.dayOfWeek.getDisplayName(TextStyle.FULL, Locale("ko", "KR"))
-
-        return date.format(outputFormatter).replace(date.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.ENGLISH), koreanDayOfWeek)
     }
 }
