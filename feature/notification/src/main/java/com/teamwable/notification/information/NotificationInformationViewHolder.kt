7d887f0acb4ce13910e3334f8893ac53566fb3c2
Eonji-sw <ejkim0625@gmail.com>
2024-08-18 19:23:10 +0900

    #28 [MOD] : viewHolder 로직 수정

--- feature/notification/src/main/java/com/teamwable/notification/information/NotificationInformationViewHolder.kt
+++ feature/notification/src/main/java/com/teamwable/notification/information/NotificationInformationViewHolder.kt
@@ -4,17 +4,27 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.teamwable.model.NotificationInformationModel
-import com.teamwable.notification.CalculateTime
 import com.teamwable.notification.R
 import com.teamwable.notification.databinding.ItemNotificationVpBinding
 import com.teamwable.ui.extensions.stringOf
+import com.teamwable.ui.util.CalculateTime
 
 class NotificationInformationViewHolder(
     private val binding: ItemNotificationVpBinding,
     private val click: (NotificationInformationModel, Int) -> Unit,
 ) : RecyclerView.ViewHolder(binding.root) {
+    private var item: NotificationInformationModel? = null
+
+    init {
+        binding.root.setOnClickListener {
+            item?.let { click(it, adapterPosition) }
+        }
+    }
+
     @RequiresApi(Build.VERSION_CODES.O)
     fun bind(data: NotificationInformationModel) {
+        item = data
+
         with(binding) {
             tvNotificationVpContent.apply {
                 when (data.infoNotificationType) {
@@ -24,11 +34,7 @@ class NotificationInformationViewHolder(
                 }
             }
 
-            tvNotificationVpTime.text = CalculateTime(root.context).getCalculateTime(data.time)
-
-            root.setOnClickListener {
-                click(data, adapterPosition)
-            }
+            tvNotificationVpTime.text = CalculateTime().getCalculateTime(root.context, data.time)
         }
     }
 }
