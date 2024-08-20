b9acde517db03da8552e53320af0c85d410e59ec
Eonji-sw <ejkim0625@gmail.com>
2024-08-20 17:02:25 +0900

    #30 [DEL] : 필요없는 코드 제거

--- core/ui/src/main/java/com/teamwable/ui/util/Constants.kt
+++ core/ui/src/main/java/com/teamwable/ui/util/Constants.kt
@@ -5,12 +5,6 @@ object BottomSheetTag {
     const val PROFILE_HAMBURGER_BOTTOM_SHEET = "ProfileHamburgerBottomSheet"
 }
 
-object DialogTag {
-    const val PROFILE_DELETE_DIALOG = "ProfileDeleteDialog"
-    const val LOGOUT_DIALOG = "LogoutDialog"
-    const val POSTING_EXIT_DIALOG = "PostingExitDialog"
-}
-
 object Arg {
     const val DIALOG_TYPE = "dialog_type"
     const val DIALOG_RESULT = "dialog_result"
