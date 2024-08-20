f803d3ac09b64c0d2c73a7429bdf5eb77d97720b
chanu <cw8881@naver.com>
2024-08-21 01:51:18 +0900

    #37 [add] : ResponseSocialLoginDto add

---
+++ core/network/src/main/java/com/teamwable/network/dto/response/auth/ResponseSocialLoginDto.kt
@@ -0,0 +1,28 @@
+package com.teamwable.network.dto.response.auth
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseSocialLoginDto(
+    @SerialName("nickName")
+    val nickName: String,
+    @SerialName("memberId")
+    val memberId: Int,
+    @SerialName("accessToken")
+    val accessToken: String,
+    @SerialName("refreshToken")
+    val refreshToken: String,
+    @SerialName("memberProfileUrl")
+    val memberProfileUrl: String,
+    @SerialName("isNewUser")
+    val isNewUser: Boolean,
+    @SerialName("isPushAlarmAllowed")
+    val isPushAlarmAllowed: Boolean?,
+    @SerialName("memberFanTeam")
+    val memberFanTeam: String,
+    @SerialName("memberLckYears")
+    val memberLckYears: Int,
+    @SerialName("memberLevel")
+    val memberLevel: Int,
+)
