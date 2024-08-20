8a7df2512a8815092d16a46d78590259271e0e27
chanu <cw8881@naver.com>
2024-08-21 02:03:02 +0900

    #37 [feat] : getReissueToken service 함수 구현

--- core/network/src/main/java/com/teamwable/network/datasource/AuthService.kt
+++ core/network/src/main/java/com/teamwable/network/datasource/AuthService.kt
@@ -1,9 +1,11 @@
 package com.teamwable.network.datasource
 
 import com.teamwable.network.dto.request.RequestSocialLoginDto
+import com.teamwable.network.dto.response.auth.ResponseReissueTokenDto
 import com.teamwable.network.dto.response.auth.ResponseSocialLoginDto
 import com.teamwable.network.util.BaseResponse
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 
@@ -14,5 +16,9 @@ interface AuthService {
         @Header("Authorization") kakaoToken: String,
     ): BaseResponse<ResponseSocialLoginDto>
 
-
+    @GET("api/v1/auth/token")
+    suspend fun getReissueToken(
+        @Header("Authorization") accessToken: String,
+        @Header("Refresh") refreshToken: String,
+    ): BaseResponse<ResponseReissueTokenDto>
 }
