0dcaf010511011ecc38e5a7e7c5a24810071263e
chanu <cw8881@naver.com>
2024-08-21 02:02:28 +0900

    #37 [feat] : postReissue 추상체 내부 함수 구현

--- core/data/src/main/java/com/teamwable/data/repository/AuthRepository.kt
+++ core/data/src/main/java/com/teamwable/data/repository/AuthRepository.kt
@@ -5,4 +5,5 @@ import com.teamwable.model.auth.UserModel
 interface AuthRepository {
     suspend fun postLogin(socialType: String, header: String): Result<UserModel>
 
+    suspend fun postReissue(headerAccess: String, headerRefresh: String): Result<String>
 }
