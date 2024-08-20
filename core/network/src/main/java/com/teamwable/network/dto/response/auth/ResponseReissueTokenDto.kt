5852d04b3b2aebac1e655a0b452a1b92033096f7
chanu <cw8881@naver.com>
2024-08-21 02:03:30 +0900

    #37 [add] : ResponseReissueTokenDto add

---
+++ core/network/src/main/java/com/teamwable/network/dto/response/auth/ResponseReissueTokenDto.kt
@@ -0,0 +1,10 @@
+package com.teamwable.network.dto.response.auth
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseReissueTokenDto(
+    @SerialName("accessToken")
+    val accessToken: String,
+)
