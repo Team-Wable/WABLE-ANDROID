2a6c07e30bfac9ef8947ebe988f40026b50bc554
chanu <cw8881@naver.com>
2024-08-21 01:24:14 +0900

    #37 [add] : setting gradle kakao maven setting

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { setUrl("https://devrepo.kakao.com/nexus/content/groups/public/") }
     }
 }
 
