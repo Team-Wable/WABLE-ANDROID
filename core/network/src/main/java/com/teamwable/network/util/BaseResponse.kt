e43c980e97b1d4d44d34750bdd14e3136a379eeb
chanu <cw8881@naver.com>
2024-08-21 01:53:58 +0900

    #37 [refact] : BaseResponse data null 제거

--- core/network/src/main/java/com/teamwable/network/util/BaseResponse.kt
+++ core/network/src/main/java/com/teamwable/network/util/BaseResponse.kt
@@ -3,7 +3,6 @@ package com.teamwable.network.util
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-// TODO:BaseResponse 수정(명세서 나온 뒤)
 @Serializable
 data class BaseResponse<T>(
     @SerialName("status")
@@ -13,5 +12,5 @@ data class BaseResponse<T>(
     @SerialName("message")
     val message: String,
     @SerialName("data")
-    val data: T? = null,
+    val data: T,
 )
