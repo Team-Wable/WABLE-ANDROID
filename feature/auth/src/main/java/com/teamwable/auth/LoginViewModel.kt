4d1ada832bd340c9e265221b549ae24674c155c3
chanu <cw8881@naver.com>
2024-08-20 14:55:01 +0900

    #34 [feat] : dialog 상태관리 viewmodel로 이전

--- feature/auth/src/main/java/com/teamwable/auth/LoginViewModel.kt
+++ feature/auth/src/main/java/com/teamwable/auth/LoginViewModel.kt
@@ -6,9 +6,12 @@ import com.teamwable.auth.model.LoginSideEffect
 import com.teamwable.data.repository.UserInfoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,6 +22,9 @@ class LoginViewModel @Inject constructor(
     private val _loginSideEffect: MutableSharedFlow<LoginSideEffect> = MutableSharedFlow()
     val loginSideEffect: SharedFlow<LoginSideEffect>
         get() = _loginSideEffect.asSharedFlow()
+
+    private val _showDialog = MutableStateFlow(false)
+    val showDialog: StateFlow<Boolean> get() = _showDialog
 
     fun observeAutoLogin() {
         viewModelScope.launch {
@@ -34,5 +40,9 @@ class LoginViewModel @Inject constructor(
         viewModelScope.launch {
             userInfoRepository.saveAutoLogin(input)
         }
+    }
+
+    fun showLoginDialog(show: Boolean) {
+        _showDialog.update { show }
     }
 }
