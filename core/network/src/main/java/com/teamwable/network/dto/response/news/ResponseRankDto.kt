6b978d0470904be8e59fb19141d3e545fd631ba8
Eonji-sw <ejkim0625@gmail.com>
2024-08-23 03:41:04 +0900

    #46 [FEAT] : news responseRankDto 구현

---
+++ core/network/src/main/java/com/teamwable/network/dto/response/news/ResponseRankDto.kt
@@ -0,0 +1,20 @@
+package com.teamwable.network.dto.response.news
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseRankDto(
+    @SerialName("teamRank")
+    val teamRank: Int = 0,
+    @SerialName("teamName")
+    val teamName: String = "",
+    @SerialName("teamWin")
+    val teamWin: Int = 0,
+    @SerialName("teamDefeat")
+    val teamDefeat: Int = 0,
+    @SerialName("winnigRate")
+    val winnigRate: Int = 0,
+    @SerialName("scoreDiff")
+    val scoreDiff: Int = 0,
+)
