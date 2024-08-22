1ea2a3f2af142ee79b7460ead2d45ff1e4998331
Sohyun <so405@naver.com>
2024-08-23 07:53:52 +0900

    #45 [FEAT] : set up basic profile

--- core/ui/src/main/java/com/teamwable/ui/extensions/GlideImageViewExt.kt
+++ core/ui/src/main/java/com/teamwable/ui/extensions/GlideImageViewExt.kt
@@ -3,14 +3,23 @@ package com.teamwable.ui.extensions
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.teamwable.ui.R
+import com.teamwable.ui.type.BasicProfileType
 
 fun ImageView.load(url: String?) {
     if (!url.isNullOrEmpty()) {
-        Glide.with(this)
-            .load(url)
-            .placeholder(R.color.gray_400)
-            .error(com.teamwable.common.R.drawable.ic_home_toast_error)
-            .into(this)
+        val profileType = BasicProfileType.entries.find { it.name == url }
+
+        if (profileType != null) {
+            Glide.with(this)
+                .load(profileType.profileDrawableRes)
+                .into(this)
+        } else {
+            Glide.with(this)
+                .load(url)
+                .placeholder(R.color.gray_400)
+                .error(com.teamwable.common.R.drawable.ic_home_toast_error)
+                .into(this)
+        }
     } else {
         setImageResource(com.teamwable.common.R.drawable.img_empty)
     }
