9cb25f0ebf9c345cd3f060550d517240b1c8106c
Eonji-sw <ejkim0625@gmail.com>
2024-08-23 06:07:27 +0900

    #47 [FEAT] : notification service 구현

---
+++ core/network/src/main/java/com/teamwable/network/datasource/NotificationService.kt
@@ -0,0 +1,25 @@
+package com.teamwable.network.datasource
+
+import com.teamwable.network.dto.response.notification.ResponseInformationDto
+import com.teamwable.network.dto.response.notification.ResponseNotificationsDto
+import com.teamwable.network.dto.response.notification.ResponseNumberDto
+import com.teamwable.network.util.BaseResponse
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.Query
+
+interface NotificationService {
+    @GET("api/v1/notification/number")
+    suspend fun getNumber(): BaseResponse<ResponseNumberDto>
+
+    @PATCH("api/v1/notification-check")
+    suspend fun patchCheck(): BaseResponse<Unit>
+
+    @GET("api/v1/notifications")
+    suspend fun getNotifications(
+        @Query(value = "cursor") notificationId: Long = -1,
+    ): BaseResponse<List<ResponseNotificationsDto>>
+
+    @GET("api/v1/notification/info/all")
+    suspend fun getInformation(): BaseResponse<List<ResponseInformationDto>>
+}
