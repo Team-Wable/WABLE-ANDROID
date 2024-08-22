eab4da18cbf75c0de27aed942f34cdb933268e3b
chanu <cw8881@naver.com>
2024-08-22 12:05:53 +0900

    #43 [feat] : firstLckWatchNavGraph 구현

---
+++ feature/onboarding/src/main/java/com/teamwable/onboarding/firstlckwatch/naviagation/FirstLckWatchNavigation.kt
@@ -0,0 +1,23 @@
+package com.teamwable.onboarding.firstlckwatch.naviagation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.teamwable.navigation.Route
+import com.teamwable.onboarding.firstlckwatch.FirstLckWatchRoute
+
+fun NavController.navigateToFirstLckWatch() {
+    this.navigate(Route.FirstLckWatch)
+}
+
+fun NavGraphBuilder.firstLckWatchNavGraph(
+    navigateToSelectLckTeam: () -> Unit,
+    onShowErrorSnackBar: (throwable: Throwable?) -> Unit,
+) {
+    composable<Route.FirstLckWatch> {
+        FirstLckWatchRoute(
+            navigateToSelectLckTeam = navigateToSelectLckTeam,
+            onShowErrorSnackBar = onShowErrorSnackBar,
+        )
+    }
+}
