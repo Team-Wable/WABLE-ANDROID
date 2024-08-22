182a037704c95a602ce1b507d65025103ac8d695
chanu <cw8881@naver.com>
2024-08-23 01:33:58 +0900

    #43 [feat] : WableCustomCardWithStroke 구현

---
+++ core/designsystem/src/main/java/com/teamwable/designsystem/component/card/WableCustomCardWithStroke.kt
@@ -0,0 +1,29 @@
+package com.teamwable.designsystem.component.card
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.teamwable.designsystem.theme.WableTheme
+
+@Composable
+fun WableCustomCardWithStroke(
+    connerRadius: Dp = 8.dp,
+    strokeWidth: Dp = 1.dp,
+    strokeColor: Color = WableTheme.colors.gray300,
+    content: @Composable () -> Unit,
+) {
+    Card(
+        shape = RoundedCornerShape(connerRadius),
+        colors = CardDefaults.cardColors(
+            containerColor = WableTheme.colors.white,
+        ),
+        border = BorderStroke(width = strokeWidth, color = strokeColor),
+    ) {
+        content()
+    }
+}
