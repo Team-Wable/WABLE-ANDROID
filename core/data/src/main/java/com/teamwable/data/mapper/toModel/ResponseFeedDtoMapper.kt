59388fb8e3e28e056e31162ff3e55d30d5f95454
Sohyun <so405@naver.com>
2024-08-23 04:57:41 +0900

    #45 [FEAT] : add profile dto, mapper

--- core/data/src/main/java/com/teamwable/data/mapper/toModel/ResponseFeedDtoMapper.kt
+++ core/data/src/main/java/com/teamwable/data/mapper/toModel/ResponseFeedDtoMapper.kt
@@ -2,6 +2,7 @@ package com.teamwable.data.mapper.toModel
 
 import com.teamwable.model.Feed
 import com.teamwable.network.dto.response.ResponseHomeFeedDto
+import com.teamwable.network.dto.response.ResponseProfileFeedDto
 
 internal fun ResponseHomeFeedDto.toFeed(): Feed =
     Feed(
@@ -18,5 +19,24 @@ internal fun ResponseHomeFeedDto.toFeed(): Feed =
         this.likedNumber.toString(),
         this.commentNumber.toString(),
         this.contentImageUrl,
+        this.memberFanTeam,
+    )
+
+// 서버랑 상의 후 나중에 사라질 함수인 듯 해서 일단 같은 파일에 만들어 놓음
+internal fun ResponseProfileFeedDto.toFeed(): Feed =
+    Feed(
+        this.memberId,
+        this.memberProfileUrl,
+        this.memberNickname,
+        this.contentId,
+        this.contentTitle,
+        this.contentText,
+        this.time,
+        this.isGhost,
+        this.memberGhost,
+        this.isLiked,
+        this.likedNumber.toString(),
+        this.commentNumber.toString(),
+        "",
         this.memberFanTeam,
     )
