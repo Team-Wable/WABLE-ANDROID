7fbea0133bcc591ffc1003f3abef076b914a9384
chanu <cw8881@naver.com>
2024-08-23 11:03:48 +0900

    #43 [delete] : fist lck watch screen 구현

--- feature/onboarding/src/main/java/com/teamwable/onboarding/firstlckwatch/FirstLckWatchScreen.kt
+++ feature/onboarding/src/main/java/com/teamwable/onboarding/firstlckwatch/FirstLckWatchScreen.kt
@@ -1,19 +1,33 @@
 package com.teamwable.onboarding.firstlckwatch
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.flowWithLifecycle
 import com.teamwable.designsystem.component.button.WableButton
 import com.teamwable.designsystem.extension.system.SetStatusBarColor
 import com.teamwable.designsystem.theme.WableTheme
+import com.teamwable.onboarding.firstlckwatch.component.WableExposedDropdownBox
 import com.teamwable.onboarding.firstlckwatch.model.FirstLckWatchSideEffect
 
 @Composable
@@ -41,24 +55,78 @@ fun FirstLckWatchRoute(
 
 @Composable
 fun FirstLckWatchScreen(
-    onNextBtnClick: () -> Unit,
+    onNextBtnClick: (String) -> Unit,
 ) {
     SetStatusBarColor(color = WableTheme.colors.white)
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        Text(text = "FirstLckWatchScreen")
+    val options = (2012..2024).map { it.toString() }
 
-        Spacer(modifier = Modifier.weight(1f))
+    var expanded by remember { mutableStateOf(false) }
+    var selectedIndex by rememberSaveable { mutableIntStateOf(options.lastIndex) }
+    val listState = rememberLazyListState()
+
+    // 드롭다운이 열릴 때 선택된 항목으로 스크롤
+    LaunchedEffect(expanded) {
+        if (expanded) {
+            listState.scrollToItem(selectedIndex)
+        }
+    }
+
+    Column(
+        verticalArrangement = Arrangement.SpaceBetween, // 상단과 하단을 공간으로 분리
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = dimensionResource(id = com.teamwable.common.R.dimen.padding_horizontal)),
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+        ) {
+            Text(
+                text = "언제부터 LCK를 시청하셨나요?",
+                style = WableTheme.typography.head00,
+                color = WableTheme.colors.black,
+                modifier = Modifier.padding(top = 16.dp),
+            )
+
+            Text(
+                text = "꼭 LCK가 아닌 LOL경기여도 좋아요",
+                style = WableTheme.typography.body02,
+                color = WableTheme.colors.gray600,
+                modifier = Modifier.padding(top = 6.dp),
+            )
+
+            Text(
+                text = "시청 시작 연도",
+                style = WableTheme.typography.caption03,
+                color = WableTheme.colors.purple50,
+                modifier = Modifier.padding(top = 34.dp),
+            )
+
+            WableExposedDropdownBox(
+                expanded = expanded,
+                options = options,
+                listState = listState,
+                onExpandedChange = { expanded = it },
+                selectedIndex = selectedIndex,
+                onSelectedIndexChange = { selectedIndex = it },
+                modifier = Modifier
+                    .padding(top = 8.dp)
+                    .fillMaxWidth()
+                    .weight(1f),
+            )
+        }
+
+        Spacer(modifier = Modifier.height(40.dp))
 
         WableButton(
             text = "다음으로",
-            onClick = onNextBtnClick,
+            onClick = { onNextBtnClick(options[selectedIndex]) },
             enabled = true,
             textStyle = WableTheme.typography.body01,
+            modifier = Modifier.padding(bottom = 24.dp),
         )
-        Spacer(modifier = Modifier.weight(1f))
     }
 }
 
