42f6e0344e42bfbb539009e31e6b17bd8029ea96
chanu <cw8881@naver.com>
2024-08-24 01:23:39 +0900

    #43 [feat] : selectLckTeamNavGraph safe args 값 받아옴

--- feature/onboarding/src/main/java/com/teamwable/onboarding/selectlckteam/naviagation/SelectLckTeamNavigation.kt
+++ feature/onboarding/src/main/java/com/teamwable/onboarding/selectlckteam/naviagation/SelectLckTeamNavigation.kt
@@ -3,21 +3,24 @@ package com.teamwable.onboarding.selectlckteam.naviagation
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 import com.teamwable.navigation.Route
 import com.teamwable.onboarding.selectlckteam.SelectLckTeamRoute
 
-fun NavController.navigateToSelectLckTeam() {
-    this.navigate(Route.SelectLckTeam)
+fun NavController.navigateToSelectLckTeam(userList: List<String>) {
+    this.navigate(Route.SelectLckTeam(userList))
 }
 
 fun NavGraphBuilder.selectLckTeamNavGraph(
     navigateToSelectLckTeam: () -> Unit,
     onShowErrorSnackBar: (throwable: Throwable?) -> Unit,
 ) {
-    composable<Route.SelectLckTeam> {
+    composable<Route.SelectLckTeam> { backStackEntry ->
+        val args = backStackEntry.toRoute<Route.SelectLckTeam>()
         SelectLckTeamRoute(
             navigateToSelectLckTeam = navigateToSelectLckTeam,
             onShowErrorSnackBar = onShowErrorSnackBar,
+            args = args,
         )
     }
 }
