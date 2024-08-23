fd05ac5be547da84d8f16753d2111eef5cc72d3a
chanu <cw8881@naver.com>
2024-08-23 10:53:30 +0900

    #43 [feat] : image button 클릭 시 rotate

--- feature/onboarding/src/main/java/com/teamwable/onboarding/firstlckwatch/component/WableExposedDropdownBox.kt
+++ feature/onboarding/src/main/java/com/teamwable/onboarding/firstlckwatch/component/WableExposedDropdownBox.kt
@@ -1,18 +1,25 @@
 package com.teamwable.onboarding.firstlckwatch.component
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -58,6 +65,7 @@ fun WableExposedDropdownBox(
                     Image(
                         painter = painterResource(id = com.teamwable.common.R.drawable.ic_sign_up_drop_down_btn),
                         contentDescription = "Custom Icon",
+                        modifier = Modifier.rotate(if (expanded) 180f else 0f),
                     )
                 }
             }
