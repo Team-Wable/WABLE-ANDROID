0f60d59864a13b023331cebf163056ae28f444b3
Eonji-sw <ejkim0625@gmail.com>
2024-08-24 04:39:22 +0900

    #48 [MOD] : profile 로그아웃 로직 추가

--- feature/profile/src/main/java/com/teamwable/profile/hamburger/ProfileHamburgerBottomSheet.kt
+++ feature/profile/src/main/java/com/teamwable/profile/hamburger/ProfileHamburgerBottomSheet.kt
@@ -2,15 +2,23 @@ package com.teamwable.profile.hamburger
 
 import android.content.Intent
 import android.net.Uri
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.teamwable.profile.ProfileViewModel
 import com.teamwable.profile.R
 import com.teamwable.profile.databinding.BottomsheetProfileHamburgerBinding
 import com.teamwable.ui.base.BindingBottomSheetFragment
 import com.teamwable.ui.component.TwoButtonDialog
 import com.teamwable.ui.type.DialogType
 import com.teamwable.ui.util.Arg.DIALOG_RESULT
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class ProfileHamburgerBottomSheet : BindingBottomSheetFragment<BottomsheetProfileHamburgerBinding>(BottomsheetProfileHamburgerBinding::inflate) {
+    private val viewModel: ProfileViewModel by viewModels()
+
     override fun initView() {
         initAccountInformationBtnClickListener()
         initNotificationSettingBtnClickListener()
@@ -48,8 +56,15 @@ class ProfileHamburgerBottomSheet : BindingBottomSheetFragment<BottomsheetProfil
 
     private fun initDialogDeleteBtnClickListener() {
         parentFragmentManager.setFragmentResultListener(DIALOG_RESULT, viewLifecycleOwner) { key, bundle ->
-            // Todo : 나중에 추가해야 함
+            viewLifecycleOwner.lifecycleScope.launch {
+                viewModel.checkLogin(false)
+                navigateToLoginScreen()
+            }
         }
+    }
+
+    private fun navigateToLoginScreen() {
+        // Todo : 구현해야 함
     }
 
     private fun navigateToWeb(uri: String) {
