f93139cc3bfc66f17f08b1d979ac7a964906ccff
chanu <cw8881@naver.com>
2024-08-24 15:25:09 +0900

    #43 [chore] : import

--- feature/onboarding/src/main/java/com/teamwable/onboarding/selectlckteam/SelectLckTeamScreen.kt
+++ feature/onboarding/src/main/java/com/teamwable/onboarding/selectlckteam/SelectLckTeamScreen.kt
@@ -1,17 +1,41 @@
 package com.teamwable.onboarding.selectlckteam
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.flowWithLifecycle
 import com.teamwable.designsystem.component.button.WableButton
-import com.teamwable.designsystem.extension.system.SetStatusBarColor
+import com.teamwable.designsystem.extension.modifier.noRippleDebounceClickable
 import com.teamwable.designsystem.theme.WableTheme
+import com.teamwable.designsystem.type.LckTeamType
+import com.teamwable.designsystem.type.MemberInfoType
+import com.teamwable.navigation.Route
+import com.teamwable.onboarding.R
+import com.teamwable.onboarding.selectlckteam.component.LckTeamItem
+import com.teamwable.onboarding.selectlckteam.model.SelectLckTeamSideEffect
 
 @Composable
 fun SelectLckTeamRoute(
