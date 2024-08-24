22b5a362126e1d1aa6bf7640441bc062871777af
chanu <cw8881@naver.com>
2024-08-24 22:24:32 +0900

    #43 [feat] : profile screen glide image 생성

--- feature/onboarding/src/main/java/com/teamwable/onboarding/profile/ProfileScreen.kt
+++ feature/onboarding/src/main/java/com/teamwable/onboarding/profile/ProfileScreen.kt
@@ -1,24 +1,35 @@
 package com.teamwable.onboarding.profile
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LocalLifecycleOwner
+import com.skydoves.landscapist.ImageOptions
+import com.skydoves.landscapist.glide.GlideImage
 import com.teamwable.designsystem.component.button.WableButton
+import com.teamwable.designsystem.extension.modifier.noRippleClickable
 import com.teamwable.designsystem.theme.WableTheme
 import com.teamwable.navigation.Route
 import com.teamwable.onboarding.R
@@ -78,6 +89,39 @@ fun ProfileScreen(
                 color = WableTheme.colors.gray600,
                 modifier = Modifier.padding(top = 6.dp),
             )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 40.dp, start = 78.dp, end = 78.dp),
+            ) {
+                GlideImage(
+                    imageModel = { },
+                    imageOptions = ImageOptions(
+                        contentScale = ContentScale.Crop,
+                        alignment = Alignment.Center,
+                    ),
+                    modifier = Modifier
+                        .aspectRatio(1f)
+                        .clip(CircleShape),
+                    previewPlaceholder = painterResource(id = com.teamwable.common.R.drawable.ic_share_profile_img_blue),
+                )
+                Image(
+                    painter = painterResource(id = com.teamwable.common.R.drawable.ic_sign_up_profile_change_btn),
+                    contentDescription = "Change Profile Image",
+                    modifier = Modifier
+                        .align(Alignment.BottomStart)
+                        .noRippleClickable {
+                        },
+                )
+                Image(
+                    painter = painterResource(id = com.teamwable.common.R.drawable.ic_sign_up_profile_plus_btn),
+                    contentDescription = "Plus Profile Image",
+                    modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .noRippleClickable { },
+                )
+            }
         }
 
         WableButton(
