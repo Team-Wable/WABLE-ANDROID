a7d721b25b1e03d929f9a621ac6c8c88fbc031bd
chanu <cw8881@naver.com>
2024-09-02 23:56:52 +0900

    #43 [feat] : ProfileViewModel 포토/갤러리 피커, 이미지 선택 로직 구현

---
+++ feature/onboarding/src/main/java/com/teamwable/onboarding/profile/ProfileViewModel.kt
@@ -0,0 +1,39 @@
+package com.teamwable.onboarding.profile
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.teamwable.onboarding.profile.model.ProfileSideEffect
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor() : ViewModel() {
+    private val _sideEffect = MutableSharedFlow<ProfileSideEffect>()
+    val sideEffect: SharedFlow<ProfileSideEffect> = _sideEffect.asSharedFlow()
+
+    private val _selectedImageUri = MutableStateFlow<String?>(null)
+    val selectedImageUri: StateFlow<String?> = _selectedImageUri
+
+    fun requestImagePicker() {
+        viewModelScope.launch {
+            _sideEffect.emit(ProfileSideEffect.RequestImagePicker)
+        }
+    }
+
+    fun onImageSelected(imageUri: String?) {
+        viewModelScope.launch {
+            _selectedImageUri.update { imageUri }
+        }
+    }
+
+    companion object {
+        private const val MIME_TYPE_IMAGE = "image/*"
+    }
+}
