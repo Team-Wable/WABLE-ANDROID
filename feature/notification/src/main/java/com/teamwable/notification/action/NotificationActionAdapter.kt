999f28d1eb9c0967f19661e68e2a26b19844c6f8
Eonji-sw <ejkim0625@gmail.com>
2024-09-04 00:57:38 +0900

    #53 [MOD] : click 인자 추가

--- feature/notification/src/main/java/com/teamwable/notification/action/NotificationActionAdapter.kt
+++ feature/notification/src/main/java/com/teamwable/notification/action/NotificationActionAdapter.kt
@@ -10,18 +10,19 @@ import com.teamwable.notification.databinding.ItemNotificationVpBinding
 import com.teamwable.ui.extensions.ItemDiffCallback
 
 class NotificationActionAdapter(
-    private val click: (NotificationActionModel, Int) -> Unit
+    private val onNotificationClick: (NotificationActionModel, Int) -> Unit,
+    private val onProfileClick: (Int) -> Unit,
 ) : PagingDataAdapter<NotificationActionModel, NotificationActionViewHolder>(
     NotificationActionAdapterDiffCallback,
 ) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationActionViewHolder {
         val binding = ItemNotificationVpBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return NotificationActionViewHolder(binding, click)
+        return NotificationActionViewHolder(binding, onNotificationClick, onProfileClick)
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: NotificationActionViewHolder, position: Int) {
-        holder.bind(getItem(position))
+        getItem(position)?.let { holder.bind(it) }
     }
 
     companion object {
