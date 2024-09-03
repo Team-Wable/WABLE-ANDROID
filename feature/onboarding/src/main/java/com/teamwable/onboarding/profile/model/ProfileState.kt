f3664bf59e92db118c5bc4c4551ca99099f0e583
chanu <cw8881@naver.com>
2024-09-04 01:54:47 +0900

    #43 [feat] : ProfileState 통합, data class 정의

---
+++ feature/onboarding/src/main/java/com/teamwable/onboarding/profile/model/ProfileState.kt
@@ -0,0 +1,13 @@
+package com.teamwable.onboarding.profile.model
+
+import androidx.compose.runtime.Immutable
+import com.teamwable.designsystem.type.NicknameType
+import com.teamwable.designsystem.type.ProfileImageType
+
+@Immutable
+data class ProfileState(
+    val nickname: String = "",
+    val textFieldType: NicknameType = NicknameType.DEFAULT,
+    val selectedImageUri: String? = null,
+    val currentImage: ProfileImageType = ProfileImageType.entries.random(),
+)
