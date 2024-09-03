0c967fe199899898c3793348a1ee6e675ec049bc
chanu <cw8881@naver.com>
2024-09-03 14:45:08 +0900

    #43 [feat] : text input,validate viewmodel에서 관리 -> usecase활용

--- feature/onboarding/src/main/java/com/teamwable/onboarding/profile/ProfileViewModel.kt
+++ feature/onboarding/src/main/java/com/teamwable/onboarding/profile/ProfileViewModel.kt
@@ -2,7 +2,9 @@ package com.teamwable.onboarding.profile
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.teamwable.designsystem.type.NicknameType
 import com.teamwable.onboarding.profile.model.ProfileSideEffect
+import com.teamwable.onboarding.profile.regex.NicknameValidationUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -14,12 +16,20 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ProfileViewModel @Inject constructor() : ViewModel() {
+class ProfileViewModel @Inject constructor(
+    private val nicknameValidationUseCase: NicknameValidationUseCase,
+) : ViewModel() {
     private val _sideEffect = MutableSharedFlow<ProfileSideEffect>()
     val sideEffect: SharedFlow<ProfileSideEffect> = _sideEffect.asSharedFlow()
 
     private val _selectedImageUri = MutableStateFlow<String?>(null)
     val selectedImageUri: StateFlow<String?> = _selectedImageUri
+
+    private val _nickname = MutableStateFlow("")
+    val nickname: StateFlow<String> = _nickname
+
+    private val _textFieldType = MutableStateFlow(NicknameType.DEFAULT)
+    val textFieldType: StateFlow<NicknameType> = _textFieldType
 
     fun requestImagePicker() {
         viewModelScope.launch {
@@ -30,6 +40,17 @@ class ProfileViewModel @Inject constructor() : ViewModel() {
     fun onImageSelected(imageUri: String?) {
         viewModelScope.launch {
             _selectedImageUri.update { imageUri }
+        }
+    }
+
+    fun onNicknameChanged(newNickname: String) {
+        _nickname.value = newNickname
+        validateNickname(newNickname)
+    }
+
+    private fun validateNickname(nickname: String) {
+        viewModelScope.launch {
+            _textFieldType.update { nicknameValidationUseCase(nickname) }
         }
     }
 }
