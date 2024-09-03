9d100d7fa8896ad5ddf4fb265ae8a97eadbb2f28
chanu <cw8881@naver.com>
2024-09-03 14:43:40 +0900

    #43 [feat] : 입력에 따른 nickname usecase 구현

---
+++ feature/onboarding/src/main/java/com/teamwable/onboarding/profile/regex/NicknameValidationUseCase.kt
@@ -0,0 +1,23 @@
+package com.teamwable.onboarding.profile.regex
+
+import com.teamwable.designsystem.type.NicknameType
+import javax.inject.Inject
+
+class NicknameValidationUseCase @Inject constructor() {
+    operator fun invoke(input: String): NicknameType = when {
+        input.isEmpty() -> NicknameType.DEFAULT
+        !isLengthValid(input) || !isOnlyAlphanumeric(input) -> NicknameType.INVALID
+        else -> NicknameType.CORRECT
+    }
+
+    private fun isOnlyAlphanumeric(input: String): Boolean =
+        input.all { it.isLetterOrDigit() }
+
+    private fun isLengthValid(input: String): Boolean =
+        input.length in MIN_LENGTH..MAX_LENGTH
+
+    companion object {
+        private const val MIN_LENGTH = 1
+        private const val MAX_LENGTH = 10
+    }
+}
