51afbf1962b30c42e7fe439629054dab3d421f7b
Sohyun <so405@naver.com>
2024-09-04 11:03:13 +0900

    #51 [FIX] : fix conflict

--- feature/profile/src/main/java/com/teamwable/profile/profile/ProfileAuthViewModel.kt
+++ feature/profile/src/main/java/com/teamwable/profile/profile/ProfileAuthViewModel.kt
@@ -29,7 +29,7 @@ class ProfileAuthViewModel @Inject constructor(
         viewModelScope.launch {
             userInfoRepository.getMemberId()
                 .map { it.toLong() }
-                .collectLatest { fetchProfileInfo(it) }
+                .collectLatest { if (it != -1L) fetchProfileInfo(it) }
         }
     }
 
