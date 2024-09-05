73691b4bc7287d1d1e843470ab2062a410ac623a
Sohyun <so405@naver.com>
2024-09-05 18:02:01 +0900

    #68 [FIX] : fix ci error

--- app/src/main/java/com/teamwable/wable/WableApp.kt
+++ app/src/main/java/com/teamwable/wable/WableApp.kt
@@ -1,17 +1,10 @@
 package com.teamwable.wable
 
 import android.app.Application
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.NotificationManager.IMPORTANCE_HIGH
 import android.content.Context
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatDelegate
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.util.Utility
-import com.teamwable.ui.util.FcmTag.CHANNEL_ID
-import com.teamwable.ui.util.FcmTag.CHANNEL_NAME
 import com.teamwable.wable.BuildConfig.KAKAO_APP_KEY
 import dagger.hilt.android.HiltAndroidApp
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -29,7 +22,6 @@ class WableApp : Application() {
         setTimber()
         setDarkMode()
         setKaKaoSdk()
-        createNotificationChannel()
     }
 
     private fun setTimber() {
@@ -45,17 +37,5 @@ class WableApp : Application() {
         KakaoSdk.init(this, KAKAO_APP_KEY)
         var keyHash = Utility.getKeyHash(this)
         Timber.tag("Kakao").d("KeyHash: $keyHash")
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun createNotificationChannel() {
-        val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager
-        val channel = NotificationChannel(
-            CHANNEL_ID,
-            CHANNEL_NAME,
-            IMPORTANCE_HIGH,
-        )
-        notificationManager?.createNotificationChannel(channel)
     }
 }
