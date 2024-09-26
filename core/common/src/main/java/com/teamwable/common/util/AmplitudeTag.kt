279a20ebe48852b05d882b0251c7886bdac24e7f
Sohyun <so405@naver.com>
2024-09-26 16:14:35 +0900

    #83 [MOD] : add amplitude tag

---
+++ core/common/src/main/java/com/teamwable/common/util/AmplitudeTag.kt
@@ -0,0 +1,71 @@
+package com.teamwable.common.util
+
+object AmplitudeTag {
+    const val CLICK_POST_UPLOAD = "click_post_upload"
+    const val CLICK_REPLY_UPLOAD = "click_reply_upload"
+    const val CLICK_POST_VIEW = "click_post_view"
+    const val CLICK_POST_LIKE = "click_post_like"
+    const val CLICK_REPLY_LIKE = "click_reply_like"
+    const val CLICK_ACCOUNT_JOIN_DONE = "click_account_join_done"
+    const val CLICK_ACCOUNT_DELETE_DONE = "click_account_delete_done"
+}
+
+object AmplitudeSignInTag {
+    const val CLICK_SIGNIN_KAKAO = "click_signin_kakao"
+    const val CLICK_AGREE_POPUP_SIGNUP = "click_agree_popup_signup"
+}
+
+object AmplitudeSignUpTag {
+    const val CLICK_NEXT_YEAR_SIGNUP = "click_next_year_signup"
+    const val CLICK_DETOUR_TEAM_SIGNUP = "click_detour_team_signup"
+    const val CLICK_NEXT_TEAM_SIGNUP = "click_next_team_signup"
+    const val CLICK_CHANGE_PICTURE_PROFILE_SIGNUP = "click_change_picture_profile_signup"
+    const val CLICK_ADD_PICTURE_PROFILE_SIGNUP = "k_add_picture_profile_signup"
+    const val CLICK_NEXT_PROFILE_SIGNUP = "click_next_profile_signup"
+    const val CLICK_COMPLETE_TNC_SIGNUP = "click_complete_tnc_signup"
+    const val CLICK_JOIN_POPUP_SIGNUP = "click_join_popup_signup"
+}
+
+object AmplitudeHomeTag {
+    const val CLICK_HOME_BOTNAVI = "click_home_botnavi"
+    const val CLICK_NEWS_BOTNAVI = "click_news_botnavi"
+    const val CLICK_NOTI_BOTNAVI = "click_noti_botnavi"
+    const val CLICK_MYPROFILE_BOTNAVI = "click_myprofile_botnavi"
+    const val CLICK_LIKE_POST = "click_like_post"
+    const val CLICK_GHOST_POST = "click_ghost_post"
+    const val CLICK_WRITE_POST = "click_write_post"
+    const val CLICK_DELETE_POST = "click_delete_post"
+    const val CLICK_WITHDRAWGHOST_POPUP = "click_withdrawghost_popup"
+    const val CLICK_APPLYGHOST_POPUP = "click_applyghost_popup"
+    const val CLICK_LIKE_COMMENT = "click_like_comment"
+    const val CLICK_GHOST_COMMENT = "click_ghost_comment"
+    const val CLICK_WRITE_COMMENT = "click_write_comment"
+    const val CLICK_GOBACK_HOME = "click_goback_home"
+}
+
+object AmplitudePostTag {
+    const val CLICK_UPLOAD_POST = "click_upload_post"
+    const val CLICK_ATTACH_PHOTO = "click_attach_photo"
+}
+
+object AmplitudeNewsTag {
+    const val CLICK_GAMESCHEDULE = "click_gameschedule"
+    const val CLICK_RANKING = "click_ranking"
+}
+
+object AmplitudeNotiTag {
+    const val CLICK_ACTIVITIESNOTI = "click_activitiesnoti"
+    const val CLICK_INFONOTI = "click_infonoti"
+}
+
+object AmplitudeProfileTag {
+    const val CLICK_WRITE_FIRSTPOST = "click_write_firstpost"
+    const val CLICK_COMPLETE_LOGOUT = "click_complete_logout"
+}
+
+object AmplitudeAuthTag {
+    const val CLICK_DELETE_ACOUNT = "click_delete_account"
+    const val CLICK_NEXT_DELETEREASON = "click_next_deletereason"
+    const val CLICK_NEXT_DELETEGUIDE = "click_next_deleteguide"
+    const val CLICK_NEXT_DELETEACCOUNT = "click_done_deleteaccount"
+}
