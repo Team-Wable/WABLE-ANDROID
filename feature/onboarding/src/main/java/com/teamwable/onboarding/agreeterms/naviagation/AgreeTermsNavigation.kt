a0f493ca908630cdef7a4fdc12e8e67e2935078f
chanu <cw8881@naver.com>
2024-10-08 00:34:54 +0900

    #91 [mod] : use custom nav type and change navcontroller parameter to custom data class

--- feature/onboarding/src/main/java/com/teamwable/onboarding/agreeterms/naviagation/AgreeTermsNavigation.kt
+++ feature/onboarding/src/main/java/com/teamwable/onboarding/agreeterms/naviagation/AgreeTermsNavigation.kt
@@ -4,18 +4,28 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
+import com.teamwable.model.profile.MemberInfoEditModel
+import com.teamwable.navigation.CustomNavType
 import com.teamwable.navigation.Route
 import com.teamwable.onboarding.agreeterms.AgreeTermsRoute
+import kotlin.reflect.typeOf
 
-fun NavController.navigateToAgreeTerms(userList: List<String>) {
-    this.navigate(Route.AgreeTerms(userList))
+fun NavController.navigateToAgreeTerms(
+    memberInfoEditModel: MemberInfoEditModel,
+    profileUri: String?,
+) {
+    this.navigate(Route.AgreeTerms(memberInfoEditModel, profileUri))
 }
 
 fun NavGraphBuilder.agreeTermsNavGraph(
     navigateToHome: () -> Unit,
     onShowErrorSnackBar: (throwable: Throwable?) -> Unit,
 ) {
-    composable<Route.AgreeTerms> { backStackEntry ->
+    composable<Route.AgreeTerms>(
+        typeMap = mapOf(
+            typeOf<MemberInfoEditModel>() to CustomNavType.createNavType<MemberInfoEditModel>(),
+        ),
+    ) { backStackEntry ->
         val args = backStackEntry.toRoute<Route.AgreeTerms>()
         AgreeTermsRoute(
             navigateToHome = navigateToHome,
