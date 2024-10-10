7274f025c7413f25f519ba8ff7b9de3bbb216d49
chanu <cw8881@naver.com>
2024-10-11 01:43:53 +0900

    #93 [add] : add custom drop shadow

--- core/designsystem/src/main/java/com/teamwable/designsystem/extension/modifier/ModifierExt.kt
+++ core/designsystem/src/main/java/com/teamwable/designsystem/extension/modifier/ModifierExt.kt
@@ -1,5 +1,6 @@
 package com.teamwable.designsystem.extension.modifier
 
+import android.graphics.BlurMaskFilter
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.Composable
@@ -10,7 +11,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawOutline
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -44,5 +54,37 @@ fun Modifier.noRippleDebounceClickable(
             }
         }
         true
+    }
+}
+
+@Composable
+fun Modifier.dropShadow(
+    shape: Shape,
+    color: Color = Color.Black.copy(alpha = 0.25f),
+    blur: Dp = 4.dp,
+    offsetY: Dp = 4.dp,
+    offsetX: Dp = 0.dp,
+    spread: Dp = 0.dp,
+) = this.drawBehind {
+    val spreadPx = spread.toPx()
+    val shadowSize = Size(size.width + spreadPx, size.height + spreadPx)
+
+    val shadowOutline = shape.createOutline(shadowSize, layoutDirection, this)
+
+    val paint = Paint().apply {
+        this.color = color
+    }
+
+    if (blur.toPx() > 0) {
+        paint.asFrameworkPaint().apply {
+            maskFilter = BlurMaskFilter(blur.toPx(), BlurMaskFilter.Blur.NORMAL)
+        }
+    }
+
+    drawIntoCanvas { canvas ->
+        canvas.save()
+        canvas.translate(offsetX.toPx() - spreadPx / 2, offsetY.toPx() - spreadPx / 2)
+        canvas.drawOutline(shadowOutline, paint)
+        canvas.restore()
     }
 }
