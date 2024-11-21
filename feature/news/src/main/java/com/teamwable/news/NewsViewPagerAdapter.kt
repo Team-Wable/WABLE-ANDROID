ee75364b0aa8894153b156baee3fcbbe6c553f65
Eonji-sw <ejkim0625@gmail.com>
2024-11-21 21:53:42 +0900

    #108 [FEAT] : news tab 추가

--- feature/news/src/main/java/com/teamwable/news/NewsViewPagerAdapter.kt
+++ feature/news/src/main/java/com/teamwable/news/NewsViewPagerAdapter.kt
@@ -3,16 +3,20 @@ package com.teamwable.news
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.teamwable.news.match.NewsMatchFragment
+import com.teamwable.news.news.NewsNewsFragment
+import com.teamwable.news.notice.NewsNoticeFragment
 import com.teamwable.news.rank.NewsRankFragment
 
 class NewsViewPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
 
-    override fun getItemCount(): Int = 2
+    override fun getItemCount(): Int = 4
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> NewsMatchFragment()
             1 -> NewsRankFragment()
+            2 -> NewsNewsFragment()
+            3 -> NewsNoticeFragment()
             else -> NewsMatchFragment()
         }
     }
