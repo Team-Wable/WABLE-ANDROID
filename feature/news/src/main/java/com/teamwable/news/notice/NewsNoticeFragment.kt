1e2aac72b130982d4e244a930711f4a62f4116c0
Eonji-sw <ejkim0625@gmail.com>
2024-11-21 21:52:45 +0900

    #108 [ADD] : news 페이지 notice tab fragment 생성

---
+++ feature/news/src/main/java/com/teamwable/news/notice/NewsNoticeFragment.kt
@@ -0,0 +1,12 @@
+package com.teamwable.news.notice
+
+import com.teamwable.news.databinding.FragmentNewsNoticeBinding
+import com.teamwable.ui.base.BindingFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NewsNoticeFragment : BindingFragment<FragmentNewsNoticeBinding>(FragmentNewsNoticeBinding::inflate) {
+    override fun initView() {
+
+    }
+}
