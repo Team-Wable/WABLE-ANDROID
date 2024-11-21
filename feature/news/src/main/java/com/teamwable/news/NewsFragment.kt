c38e7095aa7ff6d7439ccc0334a2109e9a365f5a
Eonji-sw <ejkim0625@gmail.com>
2024-11-21 21:52:21 +0900

    #108 [FEAT] : news tab 추가

--- feature/news/src/main/java/com/teamwable/news/NewsFragment.kt
+++ feature/news/src/main/java/com/teamwable/news/NewsFragment.kt
@@ -3,18 +3,42 @@ package com.teamwable.news
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.teamwable.common.util.AmplitudeNewsTag.CLICK_GAMESCHEDULE
+import com.teamwable.common.util.AmplitudeNewsTag.CLICK_NEWS
+import com.teamwable.common.util.AmplitudeNewsTag.CLICK_NOTICE
 import com.teamwable.common.util.AmplitudeNewsTag.CLICK_RANKING
 import com.teamwable.common.util.AmplitudeUtil.trackEvent
 import com.teamwable.news.databinding.FragmentNewsBinding
 import com.teamwable.ui.base.BindingFragment
+import com.teamwable.ui.extensions.colorOf
+import com.teamwable.ui.extensions.statusBarColorOf
 import com.teamwable.ui.extensions.stringOf
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class NewsFragment : BindingFragment<FragmentNewsBinding>(FragmentNewsBinding::inflate) {
     override fun initView() {
+        statusBarColorOf(com.teamwable.ui.R.color.black)
         initNewsViewPagerAdapter()
         initTabClickListener()
+
+        setBadgeOnNews(true)
+        setBadgeOnNotice(true)
+    }
+
+    private fun setBadgeOnNews(isVisible: Boolean) {
+        binding.tlNews.getTabAt(2)?.getOrCreateBadge()?.apply {
+            this.isVisible = isVisible
+            horizontalOffset = 1
+            if (isVisible) backgroundColor = colorOf(com.teamwable.ui.R.color.error) else clearNumber()
+        }
+    }
+
+    private fun setBadgeOnNotice(isVisible: Boolean) {
+        binding.tlNews.getTabAt(3)?.getOrCreateBadge()?.apply {
+            this.isVisible = isVisible
+            horizontalOffset = 1
+            if (isVisible) backgroundColor = colorOf(com.teamwable.ui.R.color.error) else clearNumber()
+        }
     }
 
     private fun initNewsViewPagerAdapter() {
@@ -24,6 +48,8 @@ class NewsFragment : BindingFragment<FragmentNewsBinding>(FragmentNewsBinding::i
                 when (position) {
                     0 -> tab.text = stringOf(R.string.tv_news_tab_match)
                     1 -> tab.text = stringOf(R.string.tv_news_tab_rank)
+                    2 -> tab.text = stringOf(R.string.tv_news_tab_news)
+                    3 -> tab.text = stringOf(R.string.tv_news_tab_notice)
                 }
             }.attach()
         }
@@ -35,6 +61,15 @@ class NewsFragment : BindingFragment<FragmentNewsBinding>(FragmentNewsBinding::i
                 when (tab?.position) {
                     0 -> trackEvent(CLICK_GAMESCHEDULE)
                     1 -> trackEvent(CLICK_RANKING)
+                    2 -> {
+                        trackEvent(CLICK_NEWS)
+                        setBadgeOnNews(false)
+                    }
+
+                    3 -> {
+                        trackEvent(CLICK_NOTICE)
+                        setBadgeOnNotice(false)
+                    }
                 }
             }
 
