8a84318e9025fdc6f03ff19d37cb2d099d69b2cf
chanu <cw8881@naver.com>
2024-11-23 22:47:41 +0900

    #113 [add] : add compose on view sysetem at news news fragment

--- feature/news/src/main/java/com/teamwable/news/news/NewsNewsFragment.kt
+++ feature/news/src/main/java/com/teamwable/news/news/NewsNewsFragment.kt
@@ -1,5 +1,7 @@
 package com.teamwable.news.news
 
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import com.teamwable.designsystem.theme.WableTheme
 import com.teamwable.news.databinding.FragmentNewsNewsBinding
 import com.teamwable.ui.base.BindingFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -7,6 +9,12 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class NewsNewsFragment : BindingFragment<FragmentNewsNewsBinding>(FragmentNewsNewsBinding::inflate) {
     override fun initView() {
-
+        binding.composeView.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                WableTheme {
+                }
+            }
+        }
     }
 }
