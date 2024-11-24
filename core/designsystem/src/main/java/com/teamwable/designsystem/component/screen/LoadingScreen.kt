ba3a3a2b9bc9f8f65ea5fe6ae751f9d63fb90048
chanu <cw8881@naver.com>
2024-11-25 06:53:02 +0900

    #115 [chore] : 불필요한 padding 제거

--- core/designsystem/src/main/java/com/teamwable/designsystem/component/screen/LoadingScreen.kt
+++ core/designsystem/src/main/java/com/teamwable/designsystem/component/screen/LoadingScreen.kt
@@ -3,11 +3,9 @@ package com.teamwable.designsystem.component.screen
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.teamwable.designsystem.component.paging.WablePagingSpinner
 
 @Composable
@@ -15,8 +13,7 @@ fun LoadingScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .aspectRatio(1f)
-            .padding(vertical = 16.dp),
+            .aspectRatio(1f),
         contentAlignment = Alignment.Center,
     ) {
         WablePagingSpinner()
