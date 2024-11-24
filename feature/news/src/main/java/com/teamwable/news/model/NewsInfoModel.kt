450ef39bfce77383b064f4b32f1a12de011ab6b5
Eonji-sw <ejkim0625@gmail.com>
2024-11-24 17:32:10 +0900

    #111 [MOD] : NewsInfoModel parcelize 적용

--- feature/news/src/main/java/com/teamwable/news/model/NewsInfoModel.kt
+++ feature/news/src/main/java/com/teamwable/news/model/NewsInfoModel.kt
@@ -1,9 +1,13 @@
 package com.teamwable.news.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class NewsInfoModel(
     val newsId: Int,
     val newsTitle: String,
     val newsText: String,
-    val newsImage: String,
+    val newsImage: String?,
     val time: String,
-)
+) : Parcelable
