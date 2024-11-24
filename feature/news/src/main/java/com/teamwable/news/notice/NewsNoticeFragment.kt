ec4ece9e1e6ac6f5629adbc29836481404d297a9
Eonji-sw <ejkim0625@gmail.com>
2024-11-24 17:33:00 +0900

    #111 [FEAT] : NewsNoticeFragment compose 적용 및 navi 함수 구현

--- feature/news/src/main/java/com/teamwable/news/notice/NewsNoticeFragment.kt
+++ feature/news/src/main/java/com/teamwable/news/notice/NewsNoticeFragment.kt
@@ -1,12 +1,34 @@
 package com.teamwable.news.notice
 
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.navigation.fragment.findNavController
+import com.teamwable.designsystem.theme.WableTheme
+import com.teamwable.news.NewsFragmentDirections
 import com.teamwable.news.databinding.FragmentNewsNoticeBinding
+import com.teamwable.news.model.NewsInfoModel
 import com.teamwable.ui.base.BindingFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class NewsNoticeFragment : BindingFragment<FragmentNewsNoticeBinding>(FragmentNewsNoticeBinding::inflate) {
     override fun initView() {
+        initComposeView()
+    }
 
+    private fun initComposeView() {
+        binding.composeNewsNotice.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                WableTheme {
+                    NewsNoticeRoute(navigateToDetail = { notice -> navigateToDetail(notice) })
+                }
+            }
+        }
+    }
+
+    private fun navigateToDetail(notice: NewsInfoModel) {
+        val parentNavController = requireParentFragment().findNavController()
+        val action = NewsFragmentDirections.actionNavigationNewsToNavigationNewsDetail(notice)
+        parentNavController.navigate(action)
     }
 }
