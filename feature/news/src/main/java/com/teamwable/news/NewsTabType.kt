030f7c2c5c89acc1b26eb52e9fcdb07f743322d0
chanu <cw8881@naver.com>
2024-11-26 03:17:46 +0900

    #119 [add] : NewsTabType string resource 추가

--- feature/news/src/main/java/com/teamwable/news/NewsTabType.kt
+++ feature/news/src/main/java/com/teamwable/news/NewsTabType.kt
@@ -2,9 +2,12 @@ package com.teamwable.news
 
 import androidx.annotation.StringRes
 
-enum class NewsTabType(@StringRes val idx: Int) {
+enum class NewsTabType(
+    val idx: Int,
+    @StringRes val title: Int = R.string.tv_news_tab_news,
+) {
     MATCH(0),
     RANK(1),
-    NEWS(2),
-    NOTICE(3),
+    NEWS(2, title = R.string.tv_news_tab_news),
+    NOTICE(3, title = R.string.tv_news_tab_notice),
 }
