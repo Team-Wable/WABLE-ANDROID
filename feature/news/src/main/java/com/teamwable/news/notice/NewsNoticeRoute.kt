03ff86985d27e02b9eace15ca29da118262c12bf
Eonji-sw <ejkim0625@gmail.com>
2024-11-25 17:44:07 +0900

    #117 [FEAT] : news notice paging api 연결

--- feature/news/src/main/java/com/teamwable/news/notice/NewsNoticeRoute.kt
+++ feature/news/src/main/java/com/teamwable/news/notice/NewsNoticeRoute.kt
@@ -1,54 +1,108 @@
 package com.teamwable.news.notice
 
-import android.content.Context
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.flowWithLifecycle
+import androidx.paging.LoadState
+import androidx.paging.PagingData
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.itemContentType
+import androidx.paging.compose.itemKey
+import com.teamwable.designsystem.component.paging.WablePagingSpinner
+import com.teamwable.designsystem.component.screen.LoadingScreen
 import com.teamwable.designsystem.component.screen.NewsNoticeEmptyScreen
 import com.teamwable.designsystem.theme.WableTheme
+import com.teamwable.designsystem.type.ContentType
 import com.teamwable.model.news.NewsInfoModel
-import com.teamwable.news.NewsViewModel
 import com.teamwable.news.R
+import com.teamwable.news.news.model.NewsInfoSideEffect
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flowOf
 
-@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 internal fun NewsNoticeRoute(
-    viewModel: NewsViewModel = hiltViewModel(),
+    viewModel: NewsNoticeViewModel = hiltViewModel(),
     navigateToDetail: (NewsInfoModel) -> Unit
 ) {
-    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val newsItems = viewModel.noticePagingFlow.collectAsLazyPagingItems()
 
-    viewModel.dummyNotice.apply {
-        if (this.isNotEmpty()) {
-            NewsNoticeScreen(context = context, notices = this, navigateToDetail = navigateToDetail)
-        } else {
-            NewsNoticeEmptyScreen(R.string.tv_news_notice_empty)
+    LaunchedEffect(lifecycleOwner) {
+        viewModel.sideEffect.flowWithLifecycle(lifecycleOwner.lifecycle)
+            .collectLatest { sideEffect ->
+                when (sideEffect) {
+                    is NewsInfoSideEffect.NavigateToDetail -> navigateToDetail(sideEffect.newsInfoModel)
+                    else -> Unit
+                }
+            }
+    }
+    NewsNoticeScreen(
+        noticeItems = newsItems,
+        onItemClick = viewModel::onItemClick
+    )
+}
+
+@Composable
+fun NewsNoticeScreen(
+    noticeItems: LazyPagingItems<NewsInfoModel>,
+    onItemClick: (NewsInfoModel) -> Unit
+) {
+    val isLoading = noticeItems.loadState.refresh is LoadState.Loading
+    val isEmpty = noticeItems.itemCount == 0 && !isLoading
+
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        when {
+            isLoading -> item { LoadingScreen() }
+            isEmpty -> item { NewsNoticeEmptyScreen(emptyTxt = R.string.tv_news_notice_empty) }
+            else -> {
+                items(
+                    count = noticeItems.itemCount,
+                    key = noticeItems.itemKey { it.newsId },
+                    contentType = noticeItems.itemContentType { ContentType.Item.name },
+                ) { idx ->
+                    noticeItems[idx]?.let {
+                        NewsNoticeItem(it, onItemClick)
+                        HorizontalDivider(
+                            thickness = 1.dp,
+                            color = WableTheme.colors.gray200,
+                        )
+                    }
+                }
+                item(
+                    key = ContentType.Spinner.name,
+                    contentType = noticeItems.itemContentType { ContentType.Spinner.name },
+                ) {
+                    if (noticeItems.loadState.append is LoadState.Loading) {
+                        WablePagingSpinner(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 16.dp),
+                        )
+                    }
+                }
+            }
         }
     }
 }
 
-@RequiresApi(Build.VERSION_CODES.O)
+@Preview(showBackground = true)
 @Composable
-fun NewsNoticeScreen(
-    context: Context,
-    notices: List<NewsInfoModel>,
-    navigateToDetail: (NewsInfoModel) -> Unit
-) {
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
-        items(items = notices, key = { item -> item.newsId }) { notice ->
-            NewsNoticeItem(context, notice, navigateToDetail)
-            HorizontalDivider(
-                thickness = 1.dp,
-                color = WableTheme.colors.gray200,
-            )
-        }
+fun GreetingPreview() {
+    WableTheme {
+        NewsNoticeScreen(
+            onItemClick = {},
+            noticeItems = flowOf(PagingData.from(emptyList<NewsInfoModel>())).collectAsLazyPagingItems(),
+        )
     }
 }
