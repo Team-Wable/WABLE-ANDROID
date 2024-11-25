96de40049cdd43f00cd9731b4e4977e25cfcd53f
Eonji-sw <ejkim0625@gmail.com>
2024-11-25 17:43:44 +0900

    #117 [MOD] : 인자 네이밍 및 변경

--- feature/news/src/main/java/com/teamwable/news/notice/NewsNoticeItem.kt
+++ feature/news/src/main/java/com/teamwable/news/notice/NewsNoticeItem.kt
@@ -1,8 +1,5 @@
 package com.teamwable.news.notice
 
-import android.content.Context
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -17,26 +14,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.teamwable.designsystem.theme.WableTheme
 import com.teamwable.model.news.NewsInfoModel
-import com.teamwable.ui.util.CalculateTime
+import com.teamwable.news.news.component.WableNewsTimeText
 
-@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun NewsNoticeItem(
-    context: Context,
     data: NewsInfoModel,
-    navigateToDetail: (NewsInfoModel) -> Unit
+    onItemClick: (NewsInfoModel) -> Unit
 ) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .background(WableTheme.colors.white)
-            .clickable { navigateToDetail(data) }
+            .clickable { onItemClick(data) }
             .padding(vertical = 12.dp, horizontal = 20.dp)
     ) {
         Row {
             Text(text = data.newsTitle, style = WableTheme.typography.body01)
             Spacer(modifier = Modifier.weight(1f))
-            Text(text = CalculateTime().getCalculateTime(context, data.time), color = WableTheme.colors.gray500, style = WableTheme.typography.caption04)
+            WableNewsTimeText(data.time)
         }
         Spacer(modifier = Modifier.height(2.dp))
         Text(text = data.newsText, color = WableTheme.colors.gray600, maxLines = 2, style = WableTheme.typography.body04)
