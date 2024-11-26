c957ec82904720858cdb25099cc42e542733d3a3
Eonji-sw <ejkim0625@gmail.com>
2024-11-26 13:06:34 +0900

    #121 [MOD] : enum class idx 제거

--- feature/news/src/main/java/com/teamwable/news/NewsTabType.kt
+++ feature/news/src/main/java/com/teamwable/news/NewsTabType.kt
@@ -1,10 +1,8 @@
 package com.teamwable.news
 
-import androidx.annotation.StringRes
-
-enum class NewsTabType(@StringRes val idx: Int) {
-    MATCH(0),
-    RANK(1),
-    NEWS(2),
-    NOTICE(3),
+enum class NewsTabType {
+    MATCH,
+    RANK,
+    NEWS,
+    NOTICE,
 }
