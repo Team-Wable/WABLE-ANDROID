5fb13cb5ea428f42fdd7f95d63a0eb01d1fc0a9c
chanu <cw8881@naver.com>
2025-02-28 19:15:09 +0900

    #145 [add] : add viewit module

---
+++ feature/viewit/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("com.teamwable.wable.feature")
+    id("com.teamwable.wable.test")
+}
+
+android {
+    namespace = "com.teamwable.viewit"
+}
+
+dependencies {
+}
---
+++ feature/viewit/src/main/java/com/teamwable/viewit/ViewItFragment.kt
@@ -0,0 +1,4 @@
+package com.teamwable.viewit
+
+class ViewItFragment {
+}
