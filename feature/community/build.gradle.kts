4c815c6158b6024b0830db6fe76d74616d10e298
chanu <cw8881@naver.com>
2025-02-28 19:19:41 +0900

    #145 [add] : add community module

---
+++ feature/community/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("com.teamwable.wable.feature")
+    id("com.teamwable.wable.test")
+    id("com.teamwable.wable.compose.feature")
+}
+
+android {
+    namespace = "com.teamwable.community"
+}
+
+dependencies {
+}
