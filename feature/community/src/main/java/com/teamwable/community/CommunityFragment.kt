bdca7610eec73968cc23954a4b88fb003198238c
chanu <cw8881@naver.com>
2025-03-02 00:32:54 +0900

    #144 [add] : add compose in view system

--- feature/community/src/main/java/com/teamwable/community/CommunityFragment.kt
+++ feature/community/src/main/java/com/teamwable/community/CommunityFragment.kt
@@ -1,4 +1,22 @@
 package com.teamwable.community
 
-class CommunityFragment {
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import com.teamwable.community.databinding.FragmentCommunityBinding
+import com.teamwable.designsystem.theme.WableTheme
+import com.teamwable.ui.base.BindingFragment
+
+class CommunityFragment : BindingFragment<FragmentCommunityBinding>(FragmentCommunityBinding::inflate) {
+    override fun initView() {
+        initComposeView()
+    }
+
+    private fun initComposeView() {
+        binding.composeView.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                WableTheme {
+                }
+            }
+        }
+    }
 }
