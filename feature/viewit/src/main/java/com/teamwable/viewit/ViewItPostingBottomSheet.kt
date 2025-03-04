6b54c4b12ee1de6ae887d3a7d7d12985198a6fd6
Sohyun <so405@naver.com>
2025-03-04 23:28:37 +0900

    #148 [FEAT] : 뷰잇 업로드 후 스낵바 띄우도록 수정

--- feature/viewit/src/main/java/com/teamwable/viewit/ViewItPostingBottomSheet.kt
+++ feature/viewit/src/main/java/com/teamwable/viewit/ViewItPostingBottomSheet.kt
@@ -27,18 +27,20 @@ class ViewItPostingBottomSheet : BindingBottomSheetFragment<BottomSheetViewItPos
     private val viewModel: ViewItPostingViewModel by viewModels()
 
     override fun initView() {
+        val viewItSnackbar = Snackbar.make(parentFragment?.view ?: return, SnackbarType.VIEW_IT_ING)
         binding.root.context.showKeyboard(binding.etViewItLinkInput)
-        collect()
+        collect(viewItSnackbar)
         setupTextWatchers()
         setOnLinkInputBtnClickListener()
-        setOnViewItUploadBtnClickListener()
+        setOnViewItUploadBtnClickListener(viewItSnackbar)
     }
 
-    private fun collect() {
+    private fun collect(snackbar: Snackbar) {
         viewLifeCycleScope.launch {
             viewModel.uiState.flowWithLifecycle(viewLifeCycle).collect { uiState ->
                 when (uiState) {
                     is ViewItPostingUiState.Success -> {
+                        snackbar.updateToCommentComplete(SnackbarType.VIEW_IT_COMPLETE)
                         parentFragmentManager.setFragmentResult(
                             POSTING_RESULT,
                             Bundle().apply { putBoolean(IS_UPLOADED, true) },
@@ -54,8 +56,7 @@ class ViewItPostingBottomSheet : BindingBottomSheetFragment<BottomSheetViewItPos
             viewModel.event.flowWithLifecycle(viewLifeCycle).collect { sideEffect ->
                 when (sideEffect) {
                     is ViewItPostingSideEffect.ShowErrorMessage -> {
-                        Snackbar.make(parentFragment?.view ?: return@collect, SnackbarType.ERROR, sideEffect.message).show()
-                        dismiss()
+                        snackbar.updateToCommentComplete(SnackbarType.ERROR, sideEffect.message)
                     }
                 }
             }
@@ -92,16 +93,29 @@ class ViewItPostingBottomSheet : BindingBottomSheetFragment<BottomSheetViewItPos
 
     private fun setOnLinkInputBtnClickListener() = with(binding) {
         btnViewItLinkInputUpload.setOnClickListener {
-            groupViewItLink.visible(false)
-            groupViewItContent.visible(true)
+            updateVisibilityBasedOnLinkStatus(true)
             etViewItLinkInputComplete.text = binding.etViewItLinkInput.text
             etViewItContentInput.requestFocus()
         }
     }
 
-    private fun setOnViewItUploadBtnClickListener() = with(binding) {
+    private fun updateVisibilityBasedOnLinkStatus(isLinkCompleted: Boolean) = with(binding) {
+        groupViewItLink.visible(!isLinkCompleted)
+        groupViewItContent.visible(isLinkCompleted)
+    }
+
+    private fun setOnViewItUploadBtnClickListener(snackbar: Snackbar) = with(binding) {
         btnViewItContentInputUpload.setOnDuplicateBlockClick {
+            snackbar.show()
             viewModel.postViewIt(etViewItLinkInputComplete.text.toString(), etViewItContentInput.text.toString())
+            clearEditText()
+            updateVisibilityBasedOnLinkStatus(false)
         }
+    }
+
+    private fun clearEditText() = with(binding) {
+        etViewItContentInput.text.clear()
+        etViewItLinkInputComplete.text.clear()
+        etViewItLinkInput.text.clear()
     }
 }
