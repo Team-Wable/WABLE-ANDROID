843c938315b9a65738f7bdfec4334e53cbf026ab
chanu <cw8881@naver.com>
2025-03-06 06:41:50 +0900

    #152 [feat] : 푸시 설정화면 딥링크 이동 로직 구현

--- feature/community/src/main/java/com/teamwable/community/CommunityFragment.kt
+++ feature/community/src/main/java/com/teamwable/community/CommunityFragment.kt
@@ -1,9 +1,12 @@
 package com.teamwable.community
 
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.navigation.fragment.findNavController
 import com.teamwable.community.databinding.FragmentCommunityBinding
 import com.teamwable.designsystem.theme.WableTheme
 import com.teamwable.ui.base.BindingFragment
+import com.teamwable.ui.extensions.DeepLinkDestination
+import com.teamwable.ui.extensions.deepLinkNavigateTo
 import com.teamwable.ui.extensions.openUri
 
 class CommunityFragment : BindingFragment<FragmentCommunityBinding>(FragmentCommunityBinding::inflate) {
@@ -18,6 +21,7 @@ class CommunityFragment : BindingFragment<FragmentCommunityBinding>(FragmentComm
                 WableTheme {
                     CommunityRoute(
                         navigateToGoogleForm = ::navigateToGoogleForm,
+                        navigateToPushAlarm = ::navigateToPushAlarm,
                     )
                 }
             }
@@ -26,5 +30,9 @@ class CommunityFragment : BindingFragment<FragmentCommunityBinding>(FragmentComm
 
     private fun navigateToGoogleForm() {
         openUri("https://forms.gle/WWfbHXvGNgXMxgZr5")
+    }
+
+    private fun navigateToPushAlarm() {
+        findNavController().deepLinkNavigateTo(requireContext(), DeepLinkDestination.PushAlarm)
     }
 }
