b1c08627a5752c5c04c679821bf6bea6ebc58c6a
chanu <cw8881@naver.com>
2025-03-06 07:03:03 +0900

    #152 [feat] : community screen state 정의

--- feature/community/src/main/java/com/teamwable/community/model/CommunityState.kt
+++ feature/community/src/main/java/com/teamwable/community/model/CommunityState.kt
@@ -1,5 +1,17 @@
 package com.teamwable.community.model
 
+import com.teamwable.common.base.BaseState
+import com.teamwable.common.type.LckTeamType
+import com.teamwable.community.component.CommunityButtonType
+import com.teamwable.designsystem.type.DialogType
+
 data class CommunityState(
-    val progress: Int = 0,
-)
+    val progress: Float = 0f,
+    val lckTeams: List<LckTeamType> = LckTeamType.entries,
+    val selectedTeamName: String? = null,
+    val preRegisterTeamName: String = "",
+    val buttonState: CommunityButtonType = CommunityButtonType.DEFAULT,
+    val dialogType: DialogType? = null,
+    val isSelected: Boolean = false,
+    val isPushPermission: Boolean = false,
+) : BaseState
