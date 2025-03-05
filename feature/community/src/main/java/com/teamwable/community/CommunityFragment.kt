1ecbd7d3fd8c71444de04150ceb1ab07540f436e
chanu <cw8881@naver.com>
2025-03-06 03:02:06 +0900

    #152 [feat] : 구글 폼 링크 이동 기능 구현

--- feature/community/src/main/java/com/teamwable/community/CommunityFragment.kt
+++ feature/community/src/main/java/com/teamwable/community/CommunityFragment.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.platform.ViewCompositionStrategy
 import com.teamwable.community.databinding.FragmentCommunityBinding
 import com.teamwable.designsystem.theme.WableTheme
 import com.teamwable.ui.base.BindingFragment
+import com.teamwable.ui.extensions.openUri
 
 class CommunityFragment : BindingFragment<FragmentCommunityBinding>(FragmentCommunityBinding::inflate) {
     override fun initView() {
@@ -15,9 +16,15 @@ class CommunityFragment : BindingFragment<FragmentCommunityBinding>(FragmentComm
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 WableTheme {
-                    CommunityRoute()
+                    CommunityRoute(
+                        navigateToGoogleForm = ::navigateToGoogleForm,
+                    )
                 }
             }
         }
+    }
+
+    private fun navigateToGoogleForm() {
+        openUri("https://forms.gle/WWfbHXvGNgXMxgZr5")
     }
 }
