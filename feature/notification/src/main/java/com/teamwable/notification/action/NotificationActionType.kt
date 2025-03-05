12fdf56aef1e907a4918fc4a5ebc58dc9dd9bac3
Sohyun <so405@naver.com>
2025-03-05 20:31:26 +0900

    #151 [FEAT] : notification viewit 분기 추가

--- feature/notification/src/main/java/com/teamwable/notification/action/NotificationActionType.kt
+++ feature/notification/src/main/java/com/teamwable/notification/action/NotificationActionType.kt
@@ -15,5 +15,6 @@ enum class NotificationActionType(@StringRes val title: Int, @StringRes val cont
     POPULAR_WRITER(title = R.string.label_notification_action_popular_writer, content = R.string.tv_notification_action_popular_writer),
     POPULAR_CONTENT(title = R.string.label_notification_action_popular_content, content = R.string.tv_notification_action_popular_content),
     CHILD_COMMENT(title = R.string.label_notification_action_feed_child_comment, content = R.string.tv_notification_action_feed_child_comment),
-    CHILD_COMMENT_LIKED(title = R.string.label_notification_action_child_comment_liked, content = R.string.tv_notification_action_chile_comment_liked)
+    CHILD_COMMENT_LIKED(title = R.string.label_notification_action_child_comment_liked, content = R.string.tv_notification_action_chile_comment_liked),
+    VIEW_IT_LIKED(title = R.string.label_notification_action_view_it_liked, content = R.string.tv_notification_action_view_it_liked),
 }
