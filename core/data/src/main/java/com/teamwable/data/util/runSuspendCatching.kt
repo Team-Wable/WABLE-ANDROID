9fad8ab96435905a78ee5084f807159132ed90e6
chanu <cw8881@naver.com>
2025-03-06 23:21:10 +0900

    #152 [fix] : fix TimeoutCancellationException for timeout cancle

--- core/data/src/main/java/com/teamwable/data/util/runSuspendCatching.kt
+++ core/data/src/main/java/com/teamwable/data/util/runSuspendCatching.kt
@@ -1,10 +1,13 @@
 package com.teamwable.data.util
 
+import kotlinx.coroutines.TimeoutCancellationException
 import kotlin.coroutines.cancellation.CancellationException
 
 inline fun <T, R> T.runSuspendCatching(block: T.() -> R): Result<R> {
     return try {
         Result.success(block())
+    } catch (t: TimeoutCancellationException) {
+        Result.failure(t)
     } catch (c: CancellationException) {
         throw c
     } catch (e: Throwable) {
