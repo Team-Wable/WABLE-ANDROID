e1e65c6278bdcf91796166d6f97e69838af08ef3
chanu <cw8881@naver.com>
2025-03-07 19:44:16 +0900

    #157 [mod] : change error message to throwable

--- feature/community/src/main/java/com/teamwable/community/model/CommunitySideEffect.kt
+++ feature/community/src/main/java/com/teamwable/community/model/CommunitySideEffect.kt
@@ -3,7 +3,7 @@ package com.teamwable.community.model
 import com.teamwable.common.base.SideEffect
 
 sealed interface CommunitySideEffect : SideEffect {
-    data class ShowSnackBar(val message: String) : CommunitySideEffect
+    data class ShowSnackBar(val message: Throwable) : CommunitySideEffect
 
     data class CopyToClipBoard(val link: String) : CommunitySideEffect
 
