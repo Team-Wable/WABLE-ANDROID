87773504a275fe00865edbf9299814c967409c5e
Sohyun <so405@naver.com>
2025-03-08 15:14:52 +0900

    #158 [FIX] : ban trigger type stirng화
    명세서에는.. viewit이었는데 알고보니 Viewit이었던 건에 대하여....

--- core/ui/src/main/java/com/teamwable/ui/type/BanTriggerType.kt
+++ core/ui/src/main/java/com/teamwable/ui/type/BanTriggerType.kt
@@ -1,7 +1,12 @@
 package com.teamwable.ui.type
 
-enum class BanTriggerType() {
-    COMMENT(),
-    CONTENT(),
-    VIEWIT(),
+import androidx.annotation.StringRes
+import com.teamwable.ui.R
+
+enum class BanTriggerType(
+    @StringRes val type: Int,
+) {
+    COMMENT(type = R.string.label_ban_trigger_type_comment),
+    CONTENT(type = R.string.label_ban_trigger_type_content),
+    VIEWIT(type = R.string.label_ban_trigger_type_viewit),
 }
