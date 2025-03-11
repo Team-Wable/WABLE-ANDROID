6e54d3f6c80dbba92bb4dd5d074720c6dfdca4bc
chanu <cw8881@naver.com>
2025-03-12 00:56:18 +0900

    #157 [add] : add fake repository

---
+++ core/domain/src/test/java/com/teamwable/domain/usecase/FakeCommunityRepository.kt
@@ -0,0 +1,19 @@
+package com.teamwable.domain.usecase
+
+import com.teamwable.data.repository.CommunityRepository
+import com.teamwable.model.community.CommunityModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+internal class FakeCommunityRepository(
+    private val communities: List<CommunityModel>,
+    private val joinedCommunity: String,
+) : CommunityRepository {
+    override fun getCommunities(): Flow<List<CommunityModel>> = flowOf(communities)
+
+    override fun getJoinedCommunity(): Flow<String> = flowOf(joinedCommunity)
+
+    override suspend fun patchPreinCommunity(communityName: String): Result<Unit> {
+        TODO("Not yet implemented")
+    }
+}
