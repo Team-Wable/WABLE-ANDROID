2bd5fd0beab7f4ecf1c5fb0df611bbf75f978717
chanu <cw8881@naver.com>
2025-03-11 22:59:09 +0900

    #157 [mod] : result -> flow로 return type 변경

--- core/data/src/main/java/com/teamwable/data/repositoryimpl/DefaultCommunityRepository.kt
+++ core/data/src/main/java/com/teamwable/data/repositoryimpl/DefaultCommunityRepository.kt
@@ -7,21 +7,25 @@ import com.teamwable.data.util.runSuspendCatching
 import com.teamwable.model.community.CommunityModel
 import com.teamwable.network.datasource.CommunityService
 import com.teamwable.network.util.handleThrowable
+import com.teamwable.network.util.toCustomError
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 internal class DefaultCommunityRepository @Inject constructor(
     private val communityService: CommunityService,
 ) : CommunityRepository {
-    override suspend fun getCommunityInfo(): Result<List<CommunityModel>> = runSuspendCatching {
-        communityService.getCommunityList().data.map { it.toCommunityModel() }
-    }.onFailure {
-        return it.handleThrowable()
+    override fun getCommunities(): Flow<List<CommunityModel>> = flow {
+        emit(communityService.getCommunityList().data.map { it.toCommunityModel() })
+    }.catch {
+        throw it.toCustomError()
     }
 
-    override suspend fun getJoinedCommunity(): Result<String> = runSuspendCatching {
-        communityService.getJoinedCommunity().data.community.orEmpty()
-    }.onFailure {
-        return it.handleThrowable()
+    override fun getJoinedCommunity(): Flow<String> = flow {
+        emit(communityService.getJoinedCommunity().data.community.orEmpty())
+    }.catch {
+        throw it.toCustomError()
     }
 
     override suspend fun patchPreinCommunity(communityName: String): Result<Unit> = runSuspendCatching {
