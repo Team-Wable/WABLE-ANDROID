cd0bc912c2bbcba6ea4e718ae8210de110a91ad0
chanu <cw8881@naver.com>
2025-03-12 22:19:15 +0900

    #161 [chore] : add stable annotation

--- feature/community/src/main/java/com/teamwable/community/model/CommunityState.kt
+++ feature/community/src/main/java/com/teamwable/community/model/CommunityState.kt
@@ -1,5 +1,6 @@
 package com.teamwable.community.model
 
+import androidx.compose.runtime.Stable
 import com.teamwable.common.base.BaseState
 import com.teamwable.common.type.LckTeamType
 import com.teamwable.community.component.CommunityButtonType
@@ -7,6 +8,7 @@ import com.teamwable.designsystem.type.DialogType
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
 
+@Stable
 data class CommunityState(
     val progress: Float = 0f,
     val lckTeams: PersistentList<LckTeamType> = persistentListOf(),
