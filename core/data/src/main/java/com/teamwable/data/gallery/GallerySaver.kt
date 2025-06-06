71bbf2f7e0ad8665f6db0f7d19bc0ec9b8a0eba6
chanu <cw8881@naver.com>
2025-06-06 21:24:10 +0900

    #167 [feat] : bitmap 갤러리에 저장하는 gallery saver class 구현

---
+++ core/data/src/main/java/com/teamwable/data/gallery/GallerySaver.kt
@@ -0,0 +1,39 @@
+package com.teamwable.data.gallery
+
+import android.content.ContentResolver
+import android.content.ContentValues
+import android.graphics.Bitmap
+import android.os.Environment
+import android.provider.MediaStore
+import javax.inject.Inject
+
+class GallerySaver @Inject constructor(
+    private val contentResolver: ContentResolver,
+) {
+    // Todo : 버전별 분기처리
+    fun saveBitmapToGallery(bitmap: Bitmap, filename: String) {
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/png")
+            put(MediaStore.MediaColumns.RELATIVE_PATH, "${Environment.DIRECTORY_PICTURES}/$FOLDER_NAME")
+            put(MediaStore.MediaColumns.IS_PENDING, 1)
+        }
+
+        val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+        if (uri != null) {
+            contentResolver.openOutputStream(uri).use { output ->
+                if (output != null) {
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, output)
+                }
+            }
+
+            contentValues.clear()
+            contentValues.put(MediaStore.MediaColumns.IS_PENDING, 0)
+            contentResolver.update(uri, contentValues, null, null)
+        }
+    }
+
+    companion object {
+        private const val FOLDER_NAME = "PokemonApp"
+    }
+}
