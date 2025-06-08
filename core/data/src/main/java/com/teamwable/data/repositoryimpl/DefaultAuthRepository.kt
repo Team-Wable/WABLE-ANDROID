fb10231f7772b5a28c96b58ff241fcd72c4720aa
chanu <cw8881@naver.com>
2025-06-08 23:40:58 +0900

    #167 [feat] : runHandledCatching를 통한 failure 블록 보일러플레이트 제거

--- core/data/src/main/java/com/teamwable/data/repositoryimpl/DefaultAuthRepository.kt
+++ core/data/src/main/java/com/teamwable/data/repositoryimpl/DefaultAuthRepository.kt
@@ -2,18 +2,16 @@ package com.teamwable.data.repositoryimpl
 
 import com.teamwable.data.mapper.toModel.toUserModel
 import com.teamwable.data.repository.AuthRepository
+import com.teamwable.data.util.runHandledCatching
 import com.teamwable.model.auth.UserModel
 import com.teamwable.network.datasource.AuthService
 import com.teamwable.network.dto.request.RequestSocialLoginDto
-import com.teamwable.network.util.handleThrowable
 import javax.inject.Inject
 
 internal class DefaultAuthRepository @Inject constructor(
     private val authService: AuthService,
 ) : AuthRepository {
-    override suspend fun postLogin(socialType: String, header: String): Result<UserModel> = runCatching {
+    override suspend fun postLogin(socialType: String, header: String): Result<UserModel> = runHandledCatching {
         authService.postLogin(RequestSocialLoginDto(socialType), header).data.toUserModel()
-    }.onFailure {
-        return it.handleThrowable()
     }
 }
--- core/data/src/main/java/com/teamwable/data/repositoryimpl/DefaultFeedImageRepository.kt
+++ core/data/src/main/java/com/teamwable/data/repositoryimpl/DefaultFeedImageRepository.kt
@@ -4,18 +4,15 @@ import com.teamwable.data.gallery.BitmapFetcher
 import com.teamwable.data.gallery.GallerySaver
 import com.teamwable.data.gallery.GallerySaver.Companion.FILE_EXTENSION
 import com.teamwable.data.repository.FeedImageRepository
-import com.teamwable.data.util.runSuspendCatching
-import com.teamwable.network.util.handleThrowable
+import com.teamwable.data.util.runHandledCatching
 import javax.inject.Inject
 
 internal class DefaultFeedImageRepository @Inject constructor(
     private val bitmapFetcher: BitmapFetcher,
     private val gallerySaver: GallerySaver,
 ) : FeedImageRepository {
-    override suspend fun saveToGallery(imageUrl: String): Result<Unit> = runSuspendCatching {
+    override suspend fun saveToGallery(imageUrl: String): Result<Unit> = runHandledCatching {
         val bitmap = bitmapFetcher.fetchBitmapFromUrl(imageUrl)
         gallerySaver.saveBitmapToGallery(bitmap, "wable_${System.currentTimeMillis()}.$FILE_EXTENSION")
-    }.onFailure {
-        return it.handleThrowable()
     }
 }
