a20649678ce23dd651ebcc1fa12c8ffc8d897ac4
chanu <cw8881@naver.com>
2025-06-08 21:34:10 +0900

    #167 [add] : add feed image repository

---
+++ core/data/src/main/java/com/teamwable/data/repositoryimpl/DefaultFeedImageRepository.kt
@@ -0,0 +1,21 @@
+package com.teamwable.data.repositoryimpl
+
+import com.teamwable.data.gallery.BitmapFetcher
+import com.teamwable.data.gallery.GallerySaver
+import com.teamwable.data.gallery.GallerySaver.Companion.FILE_EXTENSION
+import com.teamwable.data.repository.FeedImageRepository
+import com.teamwable.data.util.runSuspendCatching
+import com.teamwable.network.util.handleThrowable
+import javax.inject.Inject
+
+internal class DefaultFeedImageRepository @Inject constructor(
+    private val bitmapFetcher: BitmapFetcher,
+    private val gallerySaver: GallerySaver,
+) : FeedImageRepository {
+    override suspend fun saveToGallery(imageUrl: String): Result<Unit> = runSuspendCatching {
+        val bitmap = bitmapFetcher.fetchBitmapFromUrl(imageUrl)
+        gallerySaver.saveBitmapToGallery(bitmap, "wable_${System.currentTimeMillis()}$FILE_EXTENSION")
+    }.onFailure {
+        return it.handleThrowable()
+    }
+}
