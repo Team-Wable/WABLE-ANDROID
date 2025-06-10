6824b9a93b4bc6ae6f3727d77f21a276e1424ad2
Sohyun <so405@naver.com>
2025-06-10 13:17:00 +0900

    [UI] : 프로필 이미지 컴포넌트 로딩, 실패 상태 추가

--- feature/viewit/src/main/java/com/teamwable/viewit/component/ProfileImage.kt
+++ feature/viewit/src/main/java/com/teamwable/viewit/component/ProfileImage.kt
@@ -1,12 +1,16 @@
 package com.teamwable.viewit.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import com.skydoves.landscapist.glide.GlideImage
+import com.teamwable.designsystem.theme.WableTheme
 import com.teamwable.ui.type.BasicProfileType
 
 @Composable
@@ -14,21 +18,33 @@ fun ProfileImage(
     imageUrl: String?,
     modifier: Modifier = Modifier,
 ) {
-    if (imageUrl.isNullOrBlank()) return
-
     val profileType = BasicProfileType.entries.find { it.name == imageUrl }
 
     if (profileType != null) {
         Image(
             painter = painterResource(id = profileType.profileDrawableRes),
-            modifier = Modifier.clip(CircleShape),
+            modifier = modifier.clip(CircleShape),
             contentDescription = null,
         )
     } else {
         GlideImage(
             imageModel = { imageUrl },
             modifier = modifier.clip(CircleShape),
-            previewPlaceholder = painterResource(id = com.teamwable.common.R.drawable.img_empty),
+            loading = {
+                Box(
+                    modifier = modifier
+                        .background(WableTheme.colors.gray200)
+                        .clip(CircleShape),
+                )
+            },
+            failure = {
+                Image(
+                    painter = painterResource(id = com.teamwable.common.R.drawable.img_empty),
+                    modifier = modifier.clip(CircleShape),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = null,
+                )
+            },
         )
     }
 }
