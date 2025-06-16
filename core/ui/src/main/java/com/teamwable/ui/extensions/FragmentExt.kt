e95b5c3986e5bea39e888472c6658cd3aded9d1d
Sohyun <so405@naver.com>
2025-06-16 19:59:15 +0900

    [FEAT] : fragment result listener 확장함수 구현

--- core/ui/src/main/java/com/teamwable/ui/extensions/FragmentExt.kt
+++ core/ui/src/main/java/com/teamwable/ui/extensions/FragmentExt.kt
@@ -64,3 +64,18 @@ fun Fragment.statusBarModeOf(isLightStatusBar: Boolean = true) {
         WindowInsetsControllerCompat(this, decorView).isAppearanceLightStatusBars = isLightStatusBar
     }
 }
+
+inline fun <reified T : Enum<T>> Fragment.setupEnumResultListener(
+    key: String,
+    typeKey: String,
+    crossinline onResult: (T) -> Unit,
+) {
+    parentFragmentManager.setFragmentResultListener(
+        key,
+        viewLifecycleOwner,
+    ) { _, bundle ->
+        bundle.getString(typeKey)?.let {
+            onResult(enumValueOf<T>(it))
+        }
+    }
+}
