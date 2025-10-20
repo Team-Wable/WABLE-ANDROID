d6a13c970bdec4d56ebdbcfb4c94cd9bf03a4337
chanu <cw8881@naver.com>
2025-10-20 19:29:08 +0900

    #185 [feat] : add quiz fragment

---
+++ feature/quiz/src/main/java/com/teamwable/quiz/QuizFragment.kt
@@ -0,0 +1,24 @@
+package com.teamwable.quiz
+
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import com.teamwable.designsystem.theme.WableTheme
+import com.teamwable.quiz.databinding.FragmentQuizBinding
+import com.teamwable.ui.base.BindingFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class QuizFragment : BindingFragment<FragmentQuizBinding>(FragmentQuizBinding::inflate) {
+    override fun initView() {
+        initComposeView()
+    }
+
+    private fun initComposeView() {
+        binding.composeView.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                WableTheme {
+                }
+            }
+        }
+    }
+}
