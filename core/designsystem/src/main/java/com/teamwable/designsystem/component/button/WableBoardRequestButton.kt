fc07c6a016801e888043c43e1072debbe9a3610b
chanu <cw8881@naver.com>
2025-10-20 21:25:37 +0900

    #185 [feat] : implement BoardRequestButton and update community screen strings

---
+++ core/designsystem/src/main/java/com/teamwable/designsystem/component/button/WableBoardRequestButton.kt
@@ -0,0 +1,41 @@
+package com.teamwable.designsystem.component.button
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import com.teamwable.designsystem.R
+import com.teamwable.designsystem.theme.WableTheme
+
+@Composable
+fun BoardRequestButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    WableAnnotatedTextButton(
+        text = getAnnotatedString(),
+        onClick = onClick,
+        modifier = modifier.padding(
+            horizontal = dimensionResource(id = com.teamwable.common.R.dimen.padding_horizontal),
+            vertical = 14.dp,
+        ),
+        buttonStyle = BigButtonDefaults.blackBigButtonStyle(),
+    )
+}
+
+@Composable
+private fun getAnnotatedString(): AnnotatedString {
+    val annotatedText = buildAnnotatedString {
+        append(stringResource(R.string.str_community_floating_main))
+        withStyle(style = SpanStyle(color = WableTheme.colors.sky50)) {
+            append(stringResource(R.string.str_community_floating_sub))
+        }
+    }
+    return annotatedText
+}
