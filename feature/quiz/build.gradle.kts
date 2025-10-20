004a562bcf295ae11a852fd4b18d21cbb3847a2d
chanu <cw8881@naver.com>
2025-10-20 19:21:31 +0900

    #185 [feat] : add quiz feature module

---
+++ feature/quiz/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.teamwable.wable.feature")
+    id("com.teamwable.wable.test")
+    id("com.teamwable.wable.compose.feature")
+}
+
+android {
+    namespace = "com.teamwable.quiz"
+}
+
+dependencies {
+    implementation(project(":core:ui"))
+    implementation(project(":core:common"))
+    implementation(project(":core:model"))
+    implementation(project(":core:data"))
+
+    // AndroidX
+    implementation(libs.fragment.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+}
