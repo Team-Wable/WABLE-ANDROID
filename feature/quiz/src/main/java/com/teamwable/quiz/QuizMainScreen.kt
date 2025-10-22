945f9232635cdc33ec3617918658c381e6e632a9
chanu <cw8881@naver.com>
2025-10-23 04:13:48 +0900

    #188 [refactor] : quiz main screen 다양한 화면 대응하도록 리팩토링

--- feature/quiz/src/main/java/com/teamwable/quiz/QuizMainScreen.kt
+++ feature/quiz/src/main/java/com/teamwable/quiz/QuizMainScreen.kt
@@ -2,13 +2,14 @@ package com.teamwable.quiz
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,11 +19,10 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.teamwable.designsystem.component.button.BoardRequestButton
 import com.teamwable.designsystem.component.dot.WableDot
+import com.teamwable.designsystem.extension.preview.DevicePreviews
 import com.teamwable.designsystem.theme.WableTheme
 
 @Composable
@@ -31,61 +31,71 @@ fun QuizMainScreen() {
         modifier = Modifier
             .fillMaxSize()
             .background(color = WableTheme.colors.gray100),
+        verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Spacer(modifier = Modifier.height(54.dp))
-
-        Text(
-            text = stringResource(R.string.str_quiz_main_title),
-            style = WableTheme.typography.head00,
-            textAlign = TextAlign.Center,
-        )
-        Spacer(modifier = Modifier.height(32.dp))
-
-        Box(
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxWidth(),
-            contentAlignment = Alignment.Center,
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.img_quiz_time),
-                contentDescription = null,
-                contentScale = ContentScale.FillWidth,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp),
-            )
+            Spacer(modifier = Modifier.height(42.dp))
+
             Text(
-                text = "99:99",
+                text = stringResource(R.string.str_quiz_main_title),
+                style = WableTheme.typography.head00,
                 textAlign = TextAlign.Center,
-                fontSize = 88.sp,
             )
-        }
-        repeat(2) {
-            Spacer(modifier = Modifier.height(16.dp))
-            WableDot()
-        }
-        Spacer(modifier = Modifier.weight(1f))
+            Spacer(modifier = Modifier.height(26.dp))
 
-        Box(
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.img_quiz_time),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillWidth,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                )
+                Text(
+                    text = "99:99",
+                    style = WableTheme.typography.priceDown,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.offset(y = (-6).dp),
+                )
+            }
+        }
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxWidth(),
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.img_quiz_before),
-                contentDescription = null,
-                modifier = Modifier.aspectRatio(360 / 196f),
-            )
-            BoardRequestButton(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(horizontal = 16.dp, vertical = 20.dp),
-                onClick = {},
-            )
+            repeat(2) {
+                WableDot()
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+            Box {
+                Image(
+                    painter = painterResource(id = R.drawable.img_quiz_before),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillWidth,
+                    modifier = Modifier.fillMaxWidth(),
+                )
+                BoardRequestButton(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(horizontal = 16.dp, vertical = 20.dp),
+                    onClick = {},
+                )
+            }
         }
     }
 }
 
 @Composable
-@Preview(showBackground = true)
+@DevicePreviews
 private fun QuizMainScreenPreview() {
     WableTheme {
         QuizMainScreen()
