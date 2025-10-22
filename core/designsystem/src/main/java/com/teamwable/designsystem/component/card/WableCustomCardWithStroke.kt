ec627e3932d6ca72ae4b0ca0c3c7dca712d64f3a
chanu <cw8881@naver.com>
2025-10-23 02:41:36 +0900

    #188 [feat] : enhance WableCustomCardWithStroke to support click actions and customizable background, stroke color

--- core/designsystem/src/main/java/com/teamwable/designsystem/component/card/WableCustomCardWithStroke.kt
+++ core/designsystem/src/main/java/com/teamwable/designsystem/component/card/WableCustomCardWithStroke.kt
@@ -5,28 +5,36 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.teamwable.designsystem.extension.modifier.noRippleClickable
 import com.teamwable.designsystem.theme.WableTheme
 
 @Composable
 fun WableCustomCardWithStroke(
+    modifier: Modifier = Modifier,
     connerRadius: Dp = 8.dp,
     strokeWidth: Dp = 1.dp,
-    strokeColor: Color = WableTheme.colors.gray300,
+    strokeColor: @Composable (Boolean) -> Color = { enabled ->
+        if (enabled) WableTheme.colors.purple50 else WableTheme.colors.gray300
+    },
+    backGroundColor: Color = WableTheme.colors.white,
     enabled: Boolean = false,
+    onClick: () -> Unit = {},
     content: @Composable () -> Unit,
 ) {
     Card(
         shape = RoundedCornerShape(connerRadius),
         colors = CardDefaults.cardColors(
-            containerColor = WableTheme.colors.white,
+            containerColor = backGroundColor,
         ),
         border = BorderStroke(
             width = strokeWidth,
-            color = if (enabled) WableTheme.colors.purple50 else WableTheme.colors.gray300,
+            color = strokeColor(enabled),
         ),
+        modifier = modifier.noRippleClickable(onClick = onClick),
     ) {
         content()
     }
