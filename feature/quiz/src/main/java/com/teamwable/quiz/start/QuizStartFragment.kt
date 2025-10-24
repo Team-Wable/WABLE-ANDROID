00ead88fef3411ecfaa70a19e8bd68aec47efd57
chanu <cw8881@naver.com>
2025-10-25 04:57:43 +0900

    #192 [feat] : 화면 데이터 전달 로직 구현

--- feature/quiz/src/main/java/com/teamwable/quiz/start/QuizStartFragment.kt
+++ feature/quiz/src/main/java/com/teamwable/quiz/start/QuizStartFragment.kt
@@ -1,10 +1,15 @@
 package com.teamwable.quiz.start
 
+import android.os.Bundle
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.teamwable.designsystem.theme.WableTheme
+import com.teamwable.quiz.R
 import com.teamwable.quiz.databinding.FragmentQuizStartBinding
 import com.teamwable.ui.base.BindingFragment
+import com.teamwable.ui.component.Snackbar
+import com.teamwable.ui.type.SnackbarType
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,14 +27,22 @@ class QuizStartFragment :
                     QuizStartRoute(
                         navigateUp = { findNavController().popBackStack() },
                         navigateToResult = ::navigateToResult,
+                        onShowErrorSnackBar = ::showSnackBar,
                     )
                 }
             }
         }
     }
 
-    private fun navigateToResult() {
-        val action = QuizStartFragmentDirections.actionQuizStartFragmentToQuizResultFragment()
-        findNavController().navigate(action)
+    private fun navigateToResult(time: Int) {
+        val bundle = Bundle().apply { putInt("time", time) }
+        val navOptions = NavOptions.Builder()
+            .setPopUpTo(R.id.navigation_quiz_start, true)
+            .build()
+        findNavController().navigate(R.id.navigation_quiz_result, bundle, navOptions)
+    }
+
+    private fun showSnackBar(throwable: Throwable) {
+        Snackbar.make(binding.root, SnackbarType.ERROR, throwable).show()
     }
 }
