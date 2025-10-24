65c4ddd5bbda3bfb638752d19f17ad28ec989487
Sohyun <so405@naver.com>
2025-10-24 11:44:35 +0900

    add: curation model

---
+++ core/model/src/main/java/com/teamwable/model/news/CurationModel.kt
@@ -0,0 +1,9 @@
+package com.teamwable.model.news
+
+data class CurationModel(
+    val curationId: Long,
+    val curationLink: String,
+    val curationTitle: String,
+    val curationThumbnail: String,
+    val time: String,
+)
