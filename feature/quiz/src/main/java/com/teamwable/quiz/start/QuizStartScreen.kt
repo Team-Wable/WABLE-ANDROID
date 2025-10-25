c56d87f45bd1e985f5fb65cce667a54174373101
chanu <cw8881@naver.com>
2025-10-26 03:12:48 +0900

    #192 [refactor] : 원본 비율은 유지한채 높이만 고정하도록 코드 수정

--- feature/quiz/src/main/java/com/teamwable/quiz/start/QuizStartScreen.kt
+++ feature/quiz/src/main/java/com/teamwable/quiz/start/QuizStartScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -94,6 +95,7 @@ fun QuizStartScreen(
 
         WableGlideImage(
             imageUrl = state.quizModel.quizImage,
+            contentScale = ContentScale.Fit,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 20.dp)
