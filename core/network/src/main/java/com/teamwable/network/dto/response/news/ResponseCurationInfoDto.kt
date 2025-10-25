0dd7bed24474ea641d33187f2a776e078c298b1f
Sohyun <so405@naver.com>
2025-10-25 17:49:38 +0900

    fix : solve serialization optin issue
    https://github.com/Kotlin/kotlinx.serialization/issues/2844

--- core/network/src/main/java/com/teamwable/network/dto/response/news/ResponseCurationInfoDto.kt
+++ core/network/src/main/java/com/teamwable/network/dto/response/news/ResponseCurationInfoDto.kt
@@ -1,10 +1,8 @@
 package com.teamwable.network.dto.response.news
 
-import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-@OptIn(InternalSerializationApi::class)
 @Serializable
 data class ResponseCurationInfoDto(
     @SerialName("curationId") val curationId: Long,
