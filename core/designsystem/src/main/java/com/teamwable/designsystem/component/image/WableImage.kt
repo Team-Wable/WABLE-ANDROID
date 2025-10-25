a9f4193a690a1b43ec9c9d1e7301fff8845f24bb
chanu <cw8881@naver.com>
2025-10-26 03:33:31 +0900

    #192 [fix] : wable image error 컴포넌트 icon 도입

--- core/designsystem/src/main/java/com/teamwable/designsystem/component/image/WableImage.kt
+++ core/designsystem/src/main/java/com/teamwable/designsystem/component/image/WableImage.kt
@@ -1,7 +1,6 @@
 package com.teamwable.designsystem.component.image
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -12,8 +11,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.skydoves.landscapist.ImageOptions
 import com.skydoves.landscapist.glide.GlideImage
+import com.teamwable.designsystem.extension.composable.toImageVector
 import com.teamwable.designsystem.extension.modifier.noRippleThrottleClickable
-import com.teamwable.designsystem.theme.WableTheme
 
 @Composable
 fun WableGlideImage(
@@ -32,8 +31,10 @@ fun WableGlideImage(
         previewPlaceholder = painterResource(id = com.teamwable.common.R.drawable.img_empty),
         modifier = modifier.noRippleThrottleClickable(onClick = onClick),
         failure = {
-            Box(
-                modifier = Modifier.background(WableTheme.colors.gray200),
+            Image(
+                imageVector = toImageVector(id = com.teamwable.common.R.drawable.img_error_404),
+                contentDescription = null,
+                modifier = Modifier.align(Alignment.Center),
             )
         },
     )
